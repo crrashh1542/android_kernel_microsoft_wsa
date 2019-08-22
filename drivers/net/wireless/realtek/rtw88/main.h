cc117192ce4f7465d36833f10565a6dbdd11f462
Ping-Ke Shih <pkshih@realtek.com>
2019-08-22 22:53:03 +0800

    CHROMIUM: rtw88: sar: add SAR of TX power limit
    Originally, there are three factors of TX power:
     1) TX base power, calibrated manually and programmed in efuse.
     2) By-rate power, an offset to increase power depends on TX rate.
     3) TX limit power, an offset to contraint the max power.
    
    So, driver can get the TX power index by using:
        TX_POWER_IDX = tx_base_power + min(by-rate, limit)
    
    To take SAR into consideration, we can treat it as another limit of
    the TX power. Then driver can get the TX power by using:
        TX_POWER_IDX = tx_base_power + min(by-rate, limit, sar)
    
    Note that the values stored in driver are not presented in dBm, so
    driver needs to also convert the power limit indexes of SAR with
    tx_scale_factor, then get the difference to the TX base power.
    rtw_phy_set_tx_power_sar() will convert the values of SAR power in unit of
    0.125d Bm (sar_q3) and store them in tx_pwr_sar_{2,5}g[regd][path][rs][ch].
    
    Since certain SAR tables have single one table that isn't regulatory domain
    specific, parser can apply to all 'regd' or only one domain RTW_REGD_WW
    that SAR TX power limit can be applied no matter which regulatory domain
    is selected. Because driver get 'sar' argument by
    rtw_phy_get_tx_power_limit() with rule
        sar = is_existing(tx_pwr_sar_{2,5}g[regd]) ? tx_pwr_sar_{2,5}g[regd] :
              tx_pwr_sar_{2,5}g[RTW_REGD_WW];
    
    There are various sources of SAR table, but it is expected to adopt only
    one source. So, save current source of rtw_sar_sources to prevent more
    than one source are adopted.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Yan-Hsuan Chuang <yhchuang@realtek.com>
    (am from https://patchwork.kernel.org/patch/11370071/)
    (also found at https://lkml.kernel.org/r/20200207092844.29175-2-yhchuang@realtek.com)
    
    BUG=b:175157180
    TEST=none
    
    [brian: applied from rebase, on b/175157180]
    Change-Id: I4f50baa49a4ebd137c03fb7259e67650ae12f57d
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2596234

--- drivers/net/wireless/realtek/rtw88/main.h
+++ drivers/net/wireless/realtek/rtw88/main.h
@@ -403,6 +403,13 @@ enum rtw_wow_flags {
 	RTW_WOW_FLAG_MAX,
 };
 
+enum rtw_sar_sources {
+	RTW_SAR_SOURCE_NONE,
+	RTW_SAR_SOURCE_VNDCMD,
+	RTW_SAR_SOURCE_ACPI_STATIC,
+	RTW_SAR_SOURCE_ACPI_DYNAMIC,
+};
+
 /* the power index is represented by differences, which cck-1s & ht40-1s are
  * the base values, so for 1s's differences, there are only ht20 & ofdm
  */
@@ -1807,6 +1814,10 @@ struct rtw_fw_state {
 	u32 feature;
 };
 
+struct rtw_sar {
+	enum rtw_sar_sources source;
+};
+
 struct rtw_hal {
 	u32 rcr;
 
@@ -1852,6 +1863,10 @@ struct rtw_hal {
 			  [RTW_CHANNEL_WIDTH_MAX]
 			  [RTW_RATE_SECTION_MAX]
 			  [RTW_MAX_CHANNEL_NUM_5G];
+	s8 tx_pwr_sar_2g[RTW_REGD_MAX][RTW_RF_PATH_MAX][RTW_RATE_SECTION_MAX]
+			[RTW_MAX_CHANNEL_NUM_2G];
+	s8 tx_pwr_sar_5g[RTW_REGD_MAX][RTW_RF_PATH_MAX][RTW_RATE_SECTION_MAX]
+			[RTW_MAX_CHANNEL_NUM_5G];
 	s8 tx_pwr_tbl[RTW_RF_PATH_MAX]
 		     [DESC_RATE_MAX];
 };
@@ -1939,6 +1954,8 @@ struct rtw_dev {
 
 	bool need_rfk;
 	struct completion fw_scan_density;
+
+	struct rtw_sar sar;
 
 	/* hci related data, must be last */
 	u8 priv[] __aligned(sizeof(void *));
