fc67a6ca5b6934209dc115ae7c9000996cf281f6
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2019-10-28 17:09:09 +0200

    UPSTREAM: Bluetooth: btusb: Add support for ISO packets
    This enabled btusb driver to properly transmit ISO packets.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    (cherry picked from commit 70405f02728e657c32ccfb1d3a2f6a801a948c77)
    
    BUG=b:314685767
    TEST=build and test with LE audio local branch
    
    Change-Id: I956168f999f4e527362246c41031394beceacfe0
    Signed-off-by: Yun-hao Chung <howardchung@chromium.org>
    Signed-off-by: Jeremy Wu <jrwu@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5261440
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Ying Hsu <yinghsu@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -1858,6 +1858,13 @@ static int btusb_send_frame(struct hci_dev *hdev, struct sk_buff *skb)
 
 		hdev->stat.sco_tx++;
 		return submit_tx_urb(hdev, urb);
+
+	case HCI_ISODATA_PKT:
+		urb = alloc_bulk_urb(hdev, skb);
+		if (IS_ERR(urb))
+			return PTR_ERR(urb);
+
+		return submit_or_queue_tx_urb(hdev, urb);
 	}
 
 	return -EILSEQ;
@@ -2405,6 +2412,13 @@ static int btusb_send_frame_intel(struct hci_dev *hdev, struct sk_buff *skb)
 
 		hdev->stat.sco_tx++;
 		return submit_tx_urb(hdev, urb);
+
+	case HCI_ISODATA_PKT:
+		urb = alloc_bulk_urb(hdev, skb);
+		if (IS_ERR(urb))
+			return PTR_ERR(urb);
+
+		return submit_or_queue_tx_urb(hdev, urb);
 	}
 
 	return -EILSEQ;
