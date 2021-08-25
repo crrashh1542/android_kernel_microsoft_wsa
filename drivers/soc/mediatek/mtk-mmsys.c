fe92bc2bd3cd45f8bec1e0026bebc9f17a8eef2f
Eizan Miyamoto <eizan@chromium.org>
2021-08-25 16:33:22 +1000

    CHROMIUM: soc: mediatek: mmsys: instantiate mdp virtual device from mmsys
    A virtual device that is probed by the mtk_mdp_core driver is
    instantiated by the mtk_mmsys driver.
    
    This better reflects the logical organization of the hardware and
    driver: there are a number of hardware blocks that are used by the MDP
    that have no strict hierarchy, and the software driver is responsible
    for driving them properly.
    
    BUG=b:229583743
    TEST=Open YouTube and check chrome://media-internals
    
    Signed-off-by: Eizan Miyamoto <eizan@chromium.org>
    Reviewed-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Reviewed-by: Houlong Wei <houlong.wei@mediatek.com>
    Change-Id: Id2c54149c4e5950075d301822c069f32366ef92b
    Signed-off-by: Pin-Yen Lin <treapking@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3583713
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Commit-Queue: Hsin-Yi Wang <hsinyi@chromium.org>

--- drivers/soc/mediatek/mtk-mmsys.c
+++ drivers/soc/mediatek/mtk-mmsys.c
@@ -170,6 +170,7 @@ static int mtk_mmsys_probe(struct platform_device *pdev)
 	struct platform_device *clks;
 	struct platform_device *drm;
 	struct mtk_mmsys *mmsys;
+	struct platform_device *mdp;
 	int ret;
 
 	mmsys = devm_kzalloc(dev, sizeof(*mmsys), GFP_KERNEL);
@@ -207,10 +208,27 @@ static int mtk_mmsys_probe(struct platform_device *pdev)
 					    PLATFORM_DEVID_AUTO, NULL, 0);
 	if (IS_ERR(drm)) {
 		platform_device_unregister(clks);
-		return PTR_ERR(drm);
+		ret = PTR_ERR(drm);
+		goto err_drm;
+	}
+
+	mdp = platform_device_register_data(&pdev->dev, "mtk-mdp",
+					    PLATFORM_DEVID_AUTO, NULL, 0);
+	if (IS_ERR(mdp)) {
+		ret = PTR_ERR(mdp);
+		dev_err(dev, "Failed to register mdp: %d\n", ret);
+		goto err_mdp;
 	}
 
 	return 0;
+
+err_mdp:
+	platform_device_unregister(drm);
+
+err_drm:
+	platform_device_unregister(clks);
+
+	return ret;
 }
 
 static const struct of_device_id of_match_mtk_mmsys[] = {
