b6fb8c88d431993a4507baee38b66ee6dd8680b4
Joseph Hwang <josephsih@chromium.org>
2021-11-12 23:20:52 +0800

    CHROMIUM: Bluetooth: surface AOSP quality report through mgmt
    When receiving a HCI vendor event, the kernel checks if it is an
    AOSP bluetooth quality report. If yes, the event is sent to bluez
    user space through the mgmt socket.
    
    BUG=b:203035611,b:208921096,b:242124116
    TEST=Run an audio a2dp or hfp test and check logs.
    
    Signed-off-by: Joseph Hwang <josephsih@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3281850
    Reviewed-by: Archie Pusaka <apusaka@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3554311
    Change-Id: I2015b42d2d0a502334c9c3a2983438b89716d4f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3823820

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -34,6 +34,7 @@
 #include "hci_debugfs.h"
 #include "a2mp.h"
 #include "amp.h"
+#include "aosp.h"
 #include "smp.h"
 #include "msft.h"
 #include "eir.h"
@@ -5504,6 +5505,35 @@ static void hci_disconn_phylink_complete_evt(struct hci_dev *hdev, void *data,
 }
 #endif
 
+#define QUALITY_SPEC_NA			0x0
+#define QUALITY_SPEC_INTEL_TELEMETRY	0x1
+#define QUALITY_SPEC_AOSP_BQR		0x2
+
+static bool quality_report_evt(struct hci_dev *hdev, struct sk_buff *skb)
+{
+	if (aosp_is_quality_report_evt(skb)) {
+		if (aosp_has_quality_report(hdev) &&
+		    aosp_pull_quality_report_data(skb))
+			mgmt_quality_report(hdev, skb, QUALITY_SPEC_AOSP_BQR);
+
+		return true;
+	}
+
+	return false;
+}
+
+static void hci_vendor_evt(struct hci_dev *hdev, void *data,
+			   struct sk_buff *skb)
+{
+	/* Every specification must have a well-defined condition
+	 * to determine if an event meets the specification.
+	 * The skb is consumed by a specification only if the event
+	 * meets the specification.
+	 */
+	if (!quality_report_evt(hdev, skb))
+		msft_vendor_evt(hdev, data, skb);
+}
+
 static void le_conn_update_addr(struct hci_conn *conn, bdaddr_t *bdaddr,
 				u8 bdaddr_type, bdaddr_t *local_rpa)
 {
@@ -6893,7 +6923,7 @@ static const struct hci_ev {
 	HCI_EV(HCI_EV_NUM_COMP_BLOCKS, hci_num_comp_blocks_evt,
 	       sizeof(struct hci_ev_num_comp_blocks)),
 	/* [0xff = HCI_EV_VENDOR] */
-	HCI_EV_VL(HCI_EV_VENDOR, msft_vendor_evt, 0, HCI_MAX_EVENT_SIZE),
+	HCI_EV_VL(HCI_EV_VENDOR, hci_vendor_evt, 0, HCI_MAX_EVENT_SIZE),
 };
 
 static void hci_event_func(struct hci_dev *hdev, u8 event, struct sk_buff *skb,
