28b925e2bf68a1f5377ebc48b8153b60a7efedbd
Evan Quan <evan.quan@amd.com>
2021-11-24 11:12:31 +0800

    UPSTREAM: drm/amd/pm: drop unnecessary gfxoff controls
    Those gfxoff controls added for some specific ASICs are unnecessary.
    The functionalities are not affected without them. Also to align with
    other ASICs, they should also be dropped.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 3bce90bfbaa8de63bc500bc5a4dd262ed8e548ca)
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    
    BUG=b:235993998
    TEST=build test
    
    Backported using forklift.py
    
    Change-Id: If2d191f8e4b53c6d34731b381a8c8c2708fd02f4

--- drivers/gpu/drm/amd/pm/swsmu/smu11/smu_v11_0.c
+++ drivers/gpu/drm/amd/pm/swsmu/smu11/smu_v11_0.c
@@ -1800,7 +1800,6 @@ int smu_v11_0_set_soft_freq_limited_range(struct smu_context *smu,
 					  uint32_t min,
 					  uint32_t max)
 {
-	struct amdgpu_device *adev = smu->adev;
 	int ret = 0, clk_id = 0;
 	uint32_t param;
 
@@ -1812,9 +1811,6 @@ int smu_v11_0_set_soft_freq_limited_range(struct smu_context *smu,
 						clk_type);
 	if (clk_id < 0)
 		return clk_id;
-
-	if (clk_type == SMU_GFXCLK)
-		amdgpu_gfx_off_ctrl(adev, false);
 
 	if (max > 0) {
 		param = (uint32_t)((clk_id << 16) | (max & 0xffff));
@@ -1833,9 +1829,6 @@ int smu_v11_0_set_soft_freq_limited_range(struct smu_context *smu,
 	}
 
 out:
-	if (clk_type == SMU_GFXCLK)
-		amdgpu_gfx_off_ctrl(adev, true);
-
 	return ret;
 }
 
