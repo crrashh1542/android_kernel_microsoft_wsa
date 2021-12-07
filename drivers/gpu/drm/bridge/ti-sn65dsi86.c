192475dbf710a51bb0399bf16225c80ff5f27da4
Rob Clark <robdclark@chromium.org>
2021-12-07 13:57:52 -0800

    FROMGIT: drm/bridge: sn65dsi86: defer if there is no dsi host
    Otherwise we don't get another shot at it if the bridge probes before
    the dsi host is registered.  It seems like this is what *most* (but not
    all) of the other bridges do.
    
    It looks like this was missed in the conversion to attach dsi host at
    probe time.
    
    Fixes: c3b75d4734cb ("drm/bridge: sn65dsi86: Register and attach our DSI device at probe")
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Doug Anderson <dianders@chromium.org>
    Tested-by: Doug Anderson <dianders@chromium.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    [dianders: squashed in Stephen's simplification]
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20211207215753.635841-1-robdclark@gmail.com
    (cherry picked from commit 03848335b5b1faa4a4641fcf30b7c233579a45aa
     git://anongit.freedesktop.org/drm/drm-misc drm-misc-next)
    
    BUG=b:204483631
    TEST=With series, no more splat at boot
    
    Change-Id: I1c10290f7d2175c45fb108f3f45c3959b41f3ba4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3321781
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Rob Clark <robdclark@chromium.org>
    Tested-by: Douglas Anderson <dianders@chromium.org>

--- drivers/gpu/drm/bridge/ti-sn65dsi86.c
+++ drivers/gpu/drm/bridge/ti-sn65dsi86.c
@@ -669,7 +669,7 @@ static struct ti_sn65dsi86 *bridge_to_ti_sn65dsi86(struct drm_bridge *bridge)
 
 static int ti_sn_attach_host(struct ti_sn65dsi86 *pdata)
 {
-	int ret, val;
+	int val;
 	struct mipi_dsi_host *host;
 	struct mipi_dsi_device *dsi;
 	struct device *dev = pdata->dev;
@@ -679,16 +679,12 @@ static int ti_sn_attach_host(struct ti_sn65dsi86 *pdata)
 	};
 
 	host = of_find_mipi_dsi_host_by_node(pdata->host_node);
-	if (!host) {
-		DRM_ERROR("failed to find dsi host\n");
-		return -ENODEV;
-	}
+	if (!host)
+		return -EPROBE_DEFER;
 
 	dsi = devm_mipi_dsi_device_register_full(dev, host, &info);
-	if (IS_ERR(dsi)) {
-		DRM_ERROR("failed to create dsi device\n");
+	if (IS_ERR(dsi))
 		return PTR_ERR(dsi);
-	}
 
 	/* TODO: setting to 4 MIPI lanes always for now */
 	dsi->lanes = 4;
@@ -704,13 +700,7 @@ static int ti_sn_attach_host(struct ti_sn65dsi86 *pdata)
 
 	pdata->dsi = dsi;
 
-	ret = devm_mipi_dsi_attach(dev, dsi);
-	if (ret < 0) {
-		DRM_ERROR("failed to attach dsi to host\n");
-		return ret;
-	}
-
-	return 0;
+	return devm_mipi_dsi_attach(dev, dsi);
 }
 
 static int ti_sn_bridge_attach(struct drm_bridge *bridge,
@@ -1219,8 +1209,10 @@ static int ti_sn_bridge_probe(struct auxiliary_device *adev,
 	drm_bridge_add(&pdata->bridge);
 
 	ret = ti_sn_attach_host(pdata);
-	if (ret)
+	if (ret) {
+		dev_err_probe(pdata->dev, ret, "failed to attach dsi host\n");
 		goto err_remove_bridge;
+	}
 
 	return 0;
 
