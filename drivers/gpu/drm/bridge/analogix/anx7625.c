6baf03ed8bb6b3a3e3f8e12c4ff9b3015305f99e
Thomas Zimmermann <tzimmermann@suse.de>
2022-01-14 12:45:34 +0100

    BACKPORT: FROMGIT: drm/dp: Move public DisplayPort headers into dp/
    Move all public DisplayPort headers into dp/ and update users. No
    functional changes.
    
    v3:
    	* rebased onto latest drm-tip
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Daniel Vetter <daniel@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220114114535.29157-5-tzimmermann@suse.de
    
    (cherry picked from commit 5b529e8d9c387a34ca2b8008dc65f55d539b3ef6
     git://anongit.freedesktop.org/drm/drm-misc drm-misc-next)
    
    Conflicts:
       drivers/gpu/drm/bridge/analogix/anx7625.c
       drivers/gpu/drm/bridge/ite-it6505.c
    Conflicts because <https://crrev.com/c/3460116> and several others
    were BACKPORTs. Confirmed that:
      git grep 'include.*drm_dp' | grep -v "drm/dp/drm_dp"
    Looks OK now, at least after the next pick.
    
    BUG=b:215432805
    TEST=Future patches easier to pick
    
    Cq-Depend: chromium:3470729
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Change-Id: I8bcfa317643b2d43f2b37bd35bc09556c7b61322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3470718
    Reviewed-by: Drew Davenport <ddavenport@chromium.org>

--- drivers/gpu/drm/bridge/analogix/anx7625.c
+++ drivers/gpu/drm/bridge/analogix/anx7625.c
@@ -24,8 +24,8 @@
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_bridge.h>
 #include <drm/drm_crtc_helper.h>
-#include <drm/drm_dp_aux_bus.h>
-#include <drm/drm_dp_helper.h>
+#include <drm/dp/drm_dp_aux_bus.h>
+#include <drm/dp/drm_dp_helper.h>
 #include <drm/drm_edid.h>
 #include <drm/drm_hdcp.h>
 #include <drm/drm_mipi_dsi.h>
