2eb7702601c1b62550ecdaae3c04c01c3f58ccd0
Rob Clark <robdclark@chromium.org>
2022-02-01 08:16:12 -0800

    FROMGIT: drm/msm/gpu: Track global faults per address-space
    Other processes don't need to know about faults that they are isolated
    from by virtue of address space isolation.  They are only interested in
    whether some of their state might have been corrupted.
    
    But to be safe, also track unattributed faults.  This case should really
    never happen unless there is a kernel bug (and that would never happen,
    right?)
    
    v2: Instead of adding a new param, just change the behavior of the
        existing param to match what userspace actually wants [anholt]
    
    Closes: https://gitlab.freedesktop.org/mesa/mesa/-/issues/5934
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Link: https://lore.kernel.org/r/20220201161618.778455-3-robdclark@gmail.com
    Reviewed-by: Emma Anholt <emma@anholt.net>
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    (cherry picked from commit bc2112583a0b473f5c4ba87c00da84abd5d0b3fd
     https://gitlab.freedesktop.org/drm/msm.git msm-next)
    
    BUG=b:222511377
    TEST=emerge-trogdor chromeos-kernel-5_15
    
    Signed-off-by: Matt Turner <msturner@google.com>
    Change-Id: I2954b4b9a1684de75c4cb4a81afd61b2deec4b77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3541280
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/msm_gpu.c
+++ drivers/gpu/drm/msm/msm_gpu.c
@@ -380,8 +380,8 @@ static void recover_worker(struct kthread_work *work)
 		struct task_struct *task;
 
 		/* Increment the fault counts */
-		gpu->global_faults++;
 		submit->queue->faults++;
+		submit->aspace->faults++;
 
 		task = get_pid_task(submit->pid, PIDTYPE_PID);
 		if (task) {
@@ -399,6 +399,12 @@ static void recover_worker(struct kthread_work *work)
 		} else {
 			msm_rd_dump_submit(priv->hangrd, submit, NULL);
 		}
+	} else {
+		/*
+		 * We couldn't attribute this fault to any particular context,
+		 * so increment the global fault count instead.
+		 */
+		gpu->global_faults++;
 	}
 
 	/* Record the crash state */
