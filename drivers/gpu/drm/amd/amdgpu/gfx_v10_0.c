7bc89a8767361ca7ff95f6e413010a63286487d9
Yang Wang <KevinYang.Wang@amd.com>
2022-02-09 22:15:24 +0800

    UPSTREAM: drm/amdgpu: fix gmc init fail in sriov mode
    "adev->gfx.rlc.rlcg_reg_access_supported = true;"
    the above varible were set too late during driver initialization.
    it will cause the driver to fail to write/read register during GMC hw init
    in sriov mode.
    
    move gfx_xxx_init_rlcg_reg_access_ctrl() function to gfx early init stage
    to avoid this issue.
    
    Fixes: 5d447e29670148 ("drm/amdgpu: add helper for rlcg indirect reg access")
    
    Signed-off-by: Yang Wang <KevinYang.Wang@amd.com>
    Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 63b5fa9dbb711e245e59cc14eaae0106eb716447)
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    
    BUG=b:235993998
    TEST=build test
    
    Backported using forklift.py
    
    Change-Id: Iaf72e2048bdac75d1482e29fcefdd561d63123ce

--- drivers/gpu/drm/amd/amdgpu/gfx_v10_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v10_0.c
@@ -4384,7 +4384,6 @@ static int gfx_v10_0_rlc_init(struct amdgpu_device *adev)
 	if (adev->gfx.rlc.funcs->update_spm_vmid)
 		adev->gfx.rlc.funcs->update_spm_vmid(adev, 0xf);
 
-	gfx_v10_0_init_rlcg_reg_access_ctrl(adev);
 
 	return 0;
 }
@@ -7731,6 +7730,9 @@ static int gfx_v10_0_early_init(void *handle)
 	gfx_v10_0_set_irq_funcs(adev);
 	gfx_v10_0_set_gds_init(adev);
 	gfx_v10_0_set_rlc_funcs(adev);
+
+	/* init rlcg reg access ctrl */
+	gfx_v10_0_init_rlcg_reg_access_ctrl(adev);
 
 	return 0;
 }
