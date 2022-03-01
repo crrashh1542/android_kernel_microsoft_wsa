5c78ba9bcb384f583994f36aa9c8f6b6ea16a754
Yifan Zhang <yifan1.zhang@amd.com>
2022-03-01 18:26:43 +0800

    FROMGIT: drm/amdgpu: move amdgpu_gmc_noretry_set after ip_versions populated
    otherwise adev->ip_versions is still empty when amdgpu_gmc_noretry_set
    is called.
    
    Signed-off-by: Yifan Zhang <yifan1.zhang@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 66002dcf9239de3c99c83a84f9a0112bc5a708d4
    https://gitlab.freedesktop.org/agd5f/linux.git amd-staging-drm-next)
    
    BUG=b:233701670
    TEST=Validated boot success on skyrim
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.corp-partner.google.com>
    Change-Id: I99b1ff88d20dc1b3fa25302384ee118eb0462ec7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3665156
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -1516,7 +1516,6 @@ static int amdgpu_device_check_arguments(struct amdgpu_device *adev)
 
 	amdgpu_gmc_tmz_set(adev);
 
-	amdgpu_gmc_noretry_set(adev);
 
 	return 0;
 }
@@ -3597,6 +3596,7 @@ int amdgpu_device_init(struct amdgpu_device *adev,
 	if (r)
 		return r;
 
+	amdgpu_gmc_noretry_set(adev);
 	/* Need to get xgmi info early to decide the reset behavior*/
 	if (adev->gmc.xgmi.supported) {
 		r = adev->gfxhub.funcs->get_xgmi_info(adev);
