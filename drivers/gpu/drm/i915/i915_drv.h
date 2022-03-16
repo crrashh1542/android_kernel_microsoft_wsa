df2cd55572b43c02040a4bd0bd55030949baa2f2
Lucas De Marchi <lucas.demarchi@intel.com>
2022-03-16 16:45:37 -0700

    UPSTREAM: drm/i915: Fix renamed struct field
    Earlier versions of commit a5b7ef27da60 ("drm/i915: Add struct to hold
    IP version") named "ver" as "arch" and then when it was renamed it
    missed the rename on MEDIA_VER_FULL() since it it's currently not used.
    
    Fixes: a5b7ef27da60 ("drm/i915: Add struct to hold IP version")
    Cc: José Roberto de Souza <jose.souza@intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Reviewed-by: José Roberto de Souza <jose.souza@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220316234538.434357-1-lucas.demarchi@intel.com
    (cherry picked from commit b4ac33b973233dc08a56c8ef9d3c2edeab7a4370)
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    (cherry picked from commit 00f4150d27d2c01eaeffe1091fc311a7c0872c69
     https://anongit.freedesktop.org/git/drm-intel.git drm-intel-next)
    
    BUG=b:230301508
    TEST=emerge-nissa chromeos-kernel-5_15
    
    Signed-off-by: Sudarshan S <sudarshan.s@intel.com>
    Change-Id: I812207bc83243fd2cae135ce600d92de0f787dba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3609814
    Tested-by: Altaf Basha <altaf.basha@intel.com>
    Commit-Queue: Victor Ding <victording@chromium.org>
    Reviewed-by: Victor Ding <victording@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1328,7 +1328,7 @@ static inline struct drm_i915_private *pdev_to_i915(struct pci_dev *pdev)
 	(GRAPHICS_VER(i915) >= (from) && GRAPHICS_VER(i915) <= (until))
 
 #define MEDIA_VER(i915)			(INTEL_INFO(i915)->media.ver)
-#define MEDIA_VER_FULL(i915)		IP_VER(INTEL_INFO(i915)->media.arch, \
+#define MEDIA_VER_FULL(i915)		IP_VER(INTEL_INFO(i915)->media.ver, \
 					       INTEL_INFO(i915)->media.rel)
 #define IS_MEDIA_VER(i915, from, until) \
 	(MEDIA_VER(i915) >= (from) && MEDIA_VER(i915) <= (until))
