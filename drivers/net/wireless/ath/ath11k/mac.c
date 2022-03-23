21d6d90dcca3638d084666741d7c84efb041cc1a
Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
2022-03-23 11:14:17 +0200

    BACKPORT: ath11k: Add peer rhash table support
    When more clients (128) are connected, the UL data traffic
    KPI measurement is low compared to single client. This issue
    is due to more CPU cycles spent on the peer lookup operation
    with more clients. So reduce the peer lookup operation by
    modifying the linear based lookup operation into the rhash
    based lookup operation. This improve the peak throughput
    measurement. Since this is a software algorithm change, it is
    applicable for all the platforms.
    
    TCP UL 128 Clients test case Observation (64bit system):
    Previous: ~550 Mbps
    Now	: ~860 Mbps
    
    Tested-on: QCN9074 hw1.0 PCI WLAN.HK.2.5.0.1-01067-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/1644036628-5334-1-git-send-email-quic_periyasa@quicinc.com
    
    Conflicts:
    	drivers/net/wireless/ath/ath11k/core.h
    	drivers/net/wireless/ath/ath11k/mac.c
    There are two subsequent commits that already applied before
    this CL to fix bugs crrev/c/3891760 and crrev/c/3868376.
    Verified that this CL + crrev/c/3891760 + crrev/c/3868376 bring us
    back in line with upstream.
    
    (cherry picked from commit 7b0c70d92a435913f6e11d6a248b935697e8a3eb)
    
    BUG=b:248457431
    TEST=emerge-herobrine chromeos-kernel-5_15; deploy and test basic WiFi
         functionalities on herobrine device.
    TEST=suite:wifi_matfunc results are consistent with lab results.
    
    Signed-off-by: Jun Yu <junyuu@chromium.org>
    Change-Id: Ib71512234716744bb69427e6a95525e48d7b4c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4024228
    Reviewed-by: Kevin Lund <kglund@google.com>
    Reviewed-by: Abhishek Kumar <kuabhs@chromium.org>

--- drivers/net/wireless/ath/ath11k/mac.c
+++ drivers/net/wireless/ath/ath11k/mac.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 /*
  * Copyright (c) 2018-2019 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <net/mac80211.h>
@@ -879,13 +879,16 @@ void ath11k_mac_peer_cleanup_all(struct ath11k *ar)
 
 	lockdep_assert_held(&ar->conf_mutex);
 
+	mutex_lock(&ab->tbl_mtx_lock);
 	spin_lock_bh(&ab->base_lock);
 	list_for_each_entry_safe(peer, tmp, &ab->peers, list) {
 		ath11k_peer_rx_tid_cleanup(ar, peer);
+		ath11k_peer_rhash_delete(ab, peer);
 		list_del(&peer->list);
 		kfree(peer);
 	}
 	spin_unlock_bh(&ab->base_lock);
+	mutex_unlock(&ab->tbl_mtx_lock);
 
 	ar->num_peers = 0;
 	ar->num_stations = 0;
@@ -4577,6 +4580,7 @@ static int ath11k_mac_op_sta_state(struct ieee80211_hw *hw,
 		}
 
 		ath11k_mac_dec_num_stations(arvif, sta);
+		mutex_lock(&ar->ab->tbl_mtx_lock);
 		spin_lock_bh(&ar->ab->base_lock);
 		peer = ath11k_peer_find(ar->ab, arvif->vdev_id, sta->addr);
 		if (skip_peer_delete && peer) {
@@ -4584,12 +4588,14 @@ static int ath11k_mac_op_sta_state(struct ieee80211_hw *hw,
 		} else if (peer && peer->sta == sta) {
 			ath11k_warn(ar->ab, "Found peer entry %pM n vdev %i after it was supposedly removed\n",
 				    vif->addr, arvif->vdev_id);
+			ath11k_peer_rhash_delete(ar->ab, peer);
 			peer->sta = NULL;
 			list_del(&peer->list);
 			kfree(peer);
 			ar->num_peers--;
 		}
 		spin_unlock_bh(&ar->ab->base_lock);
+		mutex_unlock(&ar->ab->tbl_mtx_lock);
 
 		kfree(arsta->tx_stats);
 		arsta->tx_stats = NULL;
@@ -8885,6 +8891,8 @@ void ath11k_mac_unregister(struct ath11k_base *ab)
 
 		__ath11k_mac_unregister(ar);
 	}
+
+	ath11k_peer_rhash_tbl_destroy(ab);
 }
 
 static int __ath11k_mac_register(struct ath11k *ar)
@@ -9126,6 +9134,10 @@ int ath11k_mac_register(struct ath11k_base *ab)
 	ab->cc_freq_hz = IPQ8074_CC_FREQ_HERTZ;
 	ab->free_vdev_map = (1LL << (ab->num_radios * TARGET_NUM_VDEVS(ab))) - 1;
 
+	ret = ath11k_peer_rhash_tbl_init(ab);
+	if (ret)
+		return ret;
+
 	device_get_mac_address(ab->dev, mac_addr, sizeof(mac_addr));
 
 	for (i = 0; i < ab->num_radios; i++) {
@@ -9159,6 +9171,8 @@ err_cleanup:
 		ar = pdev->ar;
 		__ath11k_mac_unregister(ar);
 	}
+
+	ath11k_peer_rhash_tbl_destroy(ab);
 
 	return ret;
 }
