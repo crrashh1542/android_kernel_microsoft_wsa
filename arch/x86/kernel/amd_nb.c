44c3badb6a5d2ce6eb4fcd94348002213615211f
Muralidhara M K <muralimk@amd.com>
2022-03-24 17:57:29 +0530

    UPSTREAM: x86/amd_nb: Unexport amd_cache_northbridges()
    amd_cache_northbridges() is exported by amd_nb.c and is called by
    amd64-agp.c and amd64_edac.c modules at module_init() time so that NB
    descriptors are properly cached before those drivers can use them.
    
    However, the init_amd_nbs() initcall already does call
    amd_cache_northbridges() unconditionally and thus makes sure the NB
    descriptors are enumerated.
    
    That initcall is a fs_initcall type which is on the 5th group (starting
    from 0) of initcalls that gets run in increasing numerical order by the
    init code.
    
    The module_init() call is turned into an __initcall() in the MODULE=n
    case and those are device-level initcalls, i.e., group 6.
    
    Therefore, the northbridges caching is already finished by the time
    module initialization starts and thus the correct initialization order
    is retained.
    
    Unexport amd_cache_northbridges(), update dependent modules to
    call amd_nb_num() instead. While at it, simplify the checks in
    amd_cache_northbridges().
    
      [ bp: Heavily massage and *actually* explain why the change is ok. ]
    
    Signed-off-by: Muralidhara M K <muralimk@amd.com>
    Signed-off-by: Naveen Krishna Chatradhi <nchatrad@amd.com>
    Signed-off-by: Borislav Petkov <bp@suse.de>
    Link: https://lore.kernel.org/r/20220324122729.221765-1-nchatrad@amd.com
    (cherry picked from commit e1907d37514b8564ba18b4a768a35beee71cb011)
    
    BUG=b:241878652
    TEST=Verify TPM functionality
    
    Signed-off-by: Jan Dabros <jsd@semihalf.com>
    Change-Id: I21d62aaceb1c379e580c8138dbcdbe2d10bb9b0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3899001
    Commit-Queue: Jan Dabros <dabros@google.com>
    Tested-by: Jan Dabros <dabros@google.com>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>

--- arch/x86/kernel/amd_nb.c
+++ arch/x86/kernel/amd_nb.c
@@ -248,7 +248,7 @@ out:
 }
 EXPORT_SYMBOL_GPL(amd_df_indirect_read);
 
-int amd_cache_northbridges(void)
+static int amd_cache_northbridges(void)
 {
 	const struct pci_device_id *misc_ids = amd_nb_misc_ids;
 	const struct pci_device_id *link_ids = amd_nb_link_ids;
@@ -270,14 +270,14 @@ int amd_cache_northbridges(void)
 	}
 
 	misc = NULL;
-	while ((misc = next_northbridge(misc, misc_ids)) != NULL)
+	while ((misc = next_northbridge(misc, misc_ids)))
 		misc_count++;
 
 	if (!misc_count)
 		return -ENODEV;
 
 	root = NULL;
-	while ((root = next_northbridge(root, root_ids)) != NULL)
+	while ((root = next_northbridge(root, root_ids)))
 		root_count++;
 
 	if (root_count) {
@@ -350,7 +350,6 @@ int amd_cache_northbridges(void)
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(amd_cache_northbridges);
 
 /*
  * Ignores subdevice/subvendor but as far as I can figure out
