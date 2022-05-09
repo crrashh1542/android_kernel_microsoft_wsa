a8347a5e981843f4aa304144fdc6c9458816e32c
Baochen Qiang <quic_bqiang@quicinc.com>
2022-05-09 14:57:31 +0300

    BACKPORT: ath11k: Implement remain-on-channel support
    Add remain on channel support, it is needed in several
    scenarios such as Passpoint etc.
    
    Currently this is supported by QCA6390, WCN6855, IPQ8074,
    IPQ6018 and QCN9074.
    
    Tested-on: WCN6855 hw2.0 PCI WLAN.HSP.1.1-01720.1-QCAHSPSWPL_V1_V2_SILICONZ_LITE-1
    
    Signed-off-by: Baochen Qiang <quic_bqiang@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20220506013614.1580274-2-quic_bqiang@quicinc.com
    (cherry picked from commit 3a5627b94222c3abc7e65486e2d2c0cc0a35c140)
    
    Conflicts:
    	drivers/net/wireless/ath/ath11k/core.c
    	Previous line complete(&ar->scan.completed); was changed to
    	complete_all(&ar->scan.completed); by already committed CL
    	6d171d0188a1e485e6eb7bf70e28b8002905ea4b
    
    BUG=b:248457431, b:177043467
    TEST=emerge-herobrine chromeos-kernel-5_15; deploy and test basic WiFi
         functionalities on herobrine device.
    
    Signed-off-by: Jun Yu <junyuu@chromium.org>
    Change-Id: I60cee45d15caaad46823c30f192a7d58e462051a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3943173
    Reviewed-by: Kevin Lund <kglund@google.com>
    Reviewed-by: Abhishek Kumar <kuabhs@chromium.org>

--- drivers/net/wireless/ath/ath11k/core.c
+++ drivers/net/wireless/ath/ath11k/core.c
@@ -1530,6 +1530,7 @@ static void ath11k_core_pre_reconfigure_recovery(struct ath11k_base *ab)
 		complete(&ar->completed_11d_scan);
 		complete(&ar->scan.started);
 		complete_all(&ar->scan.completed);
+		complete(&ar->scan.on_channel);
 		complete(&ar->peer_assoc_done);
 		complete(&ar->peer_delete_done);
 		complete(&ar->install_key_done);
