6d208a701856f4cee339fc69055dadac197dfd3d
Zhengping Jiang <jiangzp@google.com>
2022-06-16 21:57:45 +0000

    Revert "CHROMIUM: Disable Remote Wakeup for RTL8822 USB"
    This reverts commit 66e22b1640bb0516052f63d9eddc51567c08daaa.
    
    BUG=none
    TEST=build
    
    Signed-off-by: Zhengping Jiang <jiangzp@google.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -3905,9 +3905,7 @@ static int btusb_suspend(struct usb_interface *intf, pm_message_t message)
 		    device_can_wakeup(&data->udev->dev))
 			data->udev->do_remote_wakeup = 1;
 		else if (!PMSG_IS_AUTO(message) &&
-			 (!device_may_wakeup(&data->udev->dev) ||
-			  test_bit(HCI_QUIRK_DISABLE_REMOTE_WAKE,
-				   &data->hdev->quirks))) {
+			 !device_may_wakeup(&data->udev->dev)) {
 			data->udev->do_remote_wakeup = 0;
 			data->udev->reset_resume = 1;
 		}
