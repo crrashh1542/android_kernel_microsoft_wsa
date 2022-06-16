629a718d3c045fb6eac8b334cf73b4f7c9aa2902
Zhengping Jiang <jiangzp@google.com>
2022-06-16 21:58:50 +0000

    Revert "Bluetooth: add quirk disabling LE Read Transmit Power"
    This reverts commit d5eeefa335af87c08b77b0dd455ddfa30489603b.
    
    BUG=none
    TEST=build
    
    Signed-off-by: Zhengping Jiang <jiangzp@google.com>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -619,8 +619,7 @@ static int hci_init3_req(struct hci_request *req, unsigned long opt)
 			hci_req_add(req, HCI_OP_LE_READ_ADV_TX_POWER, 0, NULL);
 		}
 
-		if ((hdev->commands[38] & 0x80) &&
-		    !test_bit(HCI_QUIRK_BROKEN_READ_TRANSMIT_POWER, &hdev->quirks)) {
+		if (hdev->commands[38] & 0x80) {
 			/* Read LE Min/Max Tx Power*/
 			hci_req_add(req, HCI_OP_LE_READ_TRANSMIT_POWER,
 				    0, NULL);
