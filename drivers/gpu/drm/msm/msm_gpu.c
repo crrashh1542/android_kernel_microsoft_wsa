b628a4b6a1f5989f38d6a4556d9972bedc6e2ace
Rob Clark <robdclark@chromium.org>
2022-06-18 09:11:19 -0700

    UPSTREAM: drm/msm: Don't overwrite hw fence in hw_init
    Prior to the last commit, this could result in setting the GPU
    written fence value back to an older value, if we had missed
    updating completed_fence prior to suspend.  This was mostly
    harmless as the GPU would eventually overwrite it again with
    the correct value.  But we should just not do this.  Instead
    just leave a sanity check that the fence looks plausible (in
    case the GPU scribbled on memory).
    
    Reported-by: Steev Klimaszewski <steev@kali.org>
    Fixes: 95d1deb02a9c ("drm/msm/gem: Add fenced vma unpin")
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Tested-by: Steev Klimaszewski <steev@kali.org>
    Patchwork: https://patchwork.freedesktop.org/patch/490138/
    Link: https://lore.kernel.org/r/20220618161120.3451993-2-robdclark@gmail.com
    (cherry picked from commit c8af219d18502c52319df8d4e3dcfe29a3ca31ab)
    
    BUG=b:232883991, b:187576301
    TEST=boot trogdor-arc-r
    
    Change-Id: I0a0663b9490b542a664a63ce8394eaea4ce9fab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3756495
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Rob Clark <robdclark@chromium.org>
    Auto-Submit: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Tested-by: Rob Clark <robdclark@chromium.org>

--- drivers/gpu/drm/msm/msm_gpu.c
+++ drivers/gpu/drm/msm/msm_gpu.c
@@ -572,7 +572,7 @@ static void recover_worker(struct kthread_work *work)
 		 * one more to clear the faulting submit
 		 */
 		if (ring == cur_ring)
-			fence++;
+			ring->memptrs->fence = ++fence;
 
 		msm_update_fence(ring->fctx, fence);
 	}
