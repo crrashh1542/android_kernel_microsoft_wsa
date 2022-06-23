6dc0ac131ee9a2e6044013b68dbff93c6d2073b4
Charles Keepax <ckeepax@opensource.cirrus.com>
2022-06-23 13:52:19 +0100

    UPSTREAM: ASoC: wm*: Remove now redundant non_legacy_dai_naming flag
    The ASoC core has now been changed to default to the non-legacy DAI
    naming, as such drivers using the new scheme no longer need to specify
    the non_legacy_dai_naming flag.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://lore.kernel.org/r/20220623125250.2355471-66-ckeepax@opensource.cirrus.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 02004449dbe6ec05b5b64a88824939b8fe474b82
     https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    
    BUG=b:241965784
    TEST=Test Audio use cases.
    
    Signed-off-by: SHANKAR BOYANE <shankarx.boyane@intel.com>
    Change-Id: I712883f782af882ecebcfcc78e1a9484fe83ae88

--- sound/soc/codecs/wm8962.c
+++ sound/soc/codecs/wm8962.c
@@ -3491,7 +3491,6 @@ static const struct snd_soc_component_driver soc_component_dev_wm8962 = {
 	.set_pll		= wm8962_set_fll,
 	.use_pmdown_time	= 1,
 	.endianness		= 1,
-	.non_legacy_dai_naming	= 1,
 };
 
 /* Improve power consumption for IN4 DC measurement mode */
