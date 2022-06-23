0eb8b3dc22a024b6621822afad9fd00f4e80dda3
Charles Keepax <ckeepax@opensource.cirrus.com>
2022-06-23 13:52:06 +0100

    UPSTREAM: ASoC: max*: Remove now redundant non_legacy_dai_naming flag
    The ASoC core has now been changed to default to the non-legacy DAI
    naming, as such drivers using the new scheme no longer need to specify
    the non_legacy_dai_naming flag.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Link: https://lore.kernel.org/r/20220623125250.2355471-53-ckeepax@opensource.cirrus.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit d2d3219ebe568fe4ee90ac748939304f7e05a8ec
     https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git master)
    
    BUG=b:241965784
    TEST=Test Audio use cases.
    
    Signed-off-by: SHANKAR BOYANE <shankarx.boyane@intel.com>
    Change-Id: I70052433c2c489062efbe5bc6ed4510506ba6958

--- sound/soc/codecs/max98095.c
+++ sound/soc/codecs/max98095.c
@@ -2103,7 +2103,6 @@ static const struct snd_soc_component_driver soc_component_dev_max98095 = {
 	.idle_bias_on		= 1,
 	.use_pmdown_time	= 1,
 	.endianness		= 1,
-	.non_legacy_dai_naming	= 1,
 };
 
 static int max98095_i2c_probe(struct i2c_client *i2c,
