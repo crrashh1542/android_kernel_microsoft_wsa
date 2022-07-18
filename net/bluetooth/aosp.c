fb5f39520615e76b836775882c89e3a19926b945
Joseph Hwang <josephsih@chromium.org>
2022-07-18 14:19:36 +0800

    CHROMIUM: aosp: set and clear HCI_QUALITY_REPORT
    Set and clear the HCI_QUALITY_REPORT flag in the exact enabling
    and disabling functions. This ensure that the flag always reflects
    the correct quality report state no matter how the enabling and
    disabling functions are called.
    
    BUG=b:236340454, b:242124116
    TEST=Apply the kernel patches and the corresponding bluez patch.
    The following tests passed on guybrush/dewatt with rtl8852au.
    - bluetooth_AdapterSRHealth.sr_reconnect_classic_hid test
    - bluetooth_AdapterQRHealth.qr_a2dp_test
    
    Cq-Depend: chromium:3768102
    Signed-off-by: Joseph Hwang <josephsih@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3767112
    Reviewed-by: Archie Pusaka <apusaka@chromium.org>
    Commit-Queue: Archie Pusaka <apusaka@chromium.org>
    Change-Id: I9887eaa85955381fe3f82aa077a4827f6e8af5a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3823825

--- net/bluetooth/aosp.c
+++ net/bluetooth/aosp.c
@@ -158,6 +158,8 @@ static int enable_quality_report(struct hci_dev *hdev)
 		return PTR_ERR(skb);
 	}
 
+	hci_dev_set_flag(hdev, HCI_QUALITY_REPORT);
+
 	kfree_skb(skb);
 	return 0;
 }
@@ -176,6 +178,8 @@ static int disable_quality_report(struct hci_dev *hdev)
 			   PTR_ERR(skb));
 		return PTR_ERR(skb);
 	}
+
+	hci_dev_clear_flag(hdev, HCI_QUALITY_REPORT);
 
 	kfree_skb(skb);
 	return 0;
