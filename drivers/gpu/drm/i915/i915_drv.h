09aa7363e1e3d662f20bbed85b24317579109cf0
Jani Nikula <jani.nikula@intel.com>
2022-08-17 15:19:48 +0300

    UPSTREAM: drm/i915/mtl: Meteorlake and later support DP 2.0
    Meteorlake and newer platforms support DP 2.0.
    
    Cc: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220817121948.180655-1-jani.nikula@intel.com
    (cherry picked from commit f386832509b85e567acc849cdef22843ed33a525)
    
    BUG=242077859
    TEST=i915 driver loaded successfully
    
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
    Change-Id: Icd4323a8e5803e62630cc3c77211622b1fd057a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3988229
    Reviewed-by: Basavaprasad Kanshetty <basavaprasad.kanshetty@intel.corp-partner.google.com>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Tested-by: Basavaprasad Kanshetty <basavaprasad.kanshetty@intel.corp-partner.google.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -1177,7 +1177,7 @@ IS_SUBPLATFORM(const struct drm_i915_private *i915,
 #define HAS_IPS(dev_priv)	(IS_HSW_ULT(dev_priv) || IS_BROADWELL(dev_priv))
 
 #define HAS_DP_MST(dev_priv)	(INTEL_INFO(dev_priv)->display.has_dp_mst)
-#define HAS_DP20(dev_priv)	(IS_DG2(dev_priv))
+#define HAS_DP20(dev_priv)	(IS_DG2(dev_priv) || DISPLAY_VER(dev_priv) >= 14)
 
 #define HAS_DOUBLE_BUFFERED_M_N(dev_priv)	(DISPLAY_VER(dev_priv) >= 9 || IS_BROADWELL(dev_priv))
 
