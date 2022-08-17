50e8715455058b108c29fa0ce84487aebd9152f2
Matthew Brost <matthew.brost@intel.com>
2022-08-16 19:05:10 -0700

    UPSTREAM: drm/i915/selftests: Use correct selfest calls for live tests
    This will help in an upcoming patch where the live selftest wrappers
    are extended to do more.
    
    Signed-off-by: Matthew Brost <matthew.brost@intel.com>
    Signed-off-by: Alan Previn <alan.previn.teres.alexis@intel.com>
    Reviewed-by: John Harrison <john.c.harrison@Intel.com>
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20220817020511.2180747-2-alan.previn.teres.alexis@intel.com
    (cherry picked from commit 61faec5fa66cbd1afcd5074f168f09529f8119bf)
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    
    BUG=b:265055111
    TEST=Build test
    
    Backported using forklift.py
    
    Change-Id: I80fb448343ad8da38c4c361aed726014cbc73e19

--- drivers/gpu/drm/i915/gem/selftests/i915_gem_mman.c
+++ drivers/gpu/drm/i915/gem/selftests/i915_gem_mman.c
@@ -1845,5 +1845,5 @@ int i915_gem_mman_live_selftests(struct drm_i915_private *i915)
 		SUBTEST(igt_mmap_gpu),
 	};
 
-	return i915_subtests(tests, i915);
+	return i915_live_subtests(tests, i915);
 }
