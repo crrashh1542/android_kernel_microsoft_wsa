a5a5464c66ab90992c071638f897a2d176ed5d77
Johannes Berg <johannes.berg@intel.com>
2022-08-30 13:49:28 +0200

    CHROMIUM: iwl7000: mac80211: implement link switching
    Implement an API function and debugfs file to switch
    active links.
    
    Also provide an async version of the API so drivers
    can call it in arbitrary contexts, e.g. while in the
    authorized callback.
    
    Change-Id: I92385ba882ec984a9a2ad18293173436657e82aa
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: e357945bc78e70087e6d153b3c7768b2d29f7a07

--- drivers/net/wireless/iwl7000/mac80211/ieee80211_i.h
+++ drivers/net/wireless/iwl7000/mac80211/ieee80211_i.h
@@ -1084,6 +1084,10 @@ struct ieee80211_sub_if_data {
 	struct ieee80211_link_data deflink;
 	struct ieee80211_link_data __rcu *link[IEEE80211_MLD_MAX_NUM_LINKS];
 
+	/* for ieee80211_set_active_links_async() */
+	struct work_struct activate_links_work;
+	u16 desired_active_links;
+
 #ifdef CPTCFG_MAC80211_DEBUGFS
 	struct {
 		struct dentry *subdir_stations;
