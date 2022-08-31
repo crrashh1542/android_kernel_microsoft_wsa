c36ccd08dc863912cba78a388aaaf1e0c5d3dda4
Alon Giladi <alon.giladi@intel.com>
2022-08-31 16:36:51 +0300

    CHROMIUM: iwl7000: Generalize the parsing of the pnvm image
    Generalize iwl_pnvm_parse().This saves us from copying each payload
    twice (first in the parsing and later when copying it to the dram).
    Moreover, its  more compatible for handling larger pnvm tables in
    the future (in which payloads won't be concatenated).
    
    The main changes are:
    
    1. Take out the concatenating of the payloads from the parsing level
    2. Start using iwl_pnvm_image structure that will hold pointers to
    payloads that should be delivered to fw, their sizes and number.
    
    Signed-off-by: Alon Giladi <alon.giladi@intel.com>
    Change-Id: I1fa62997c3deb8cc6192db30810ca8451ca5ca0d
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 442ef19c2900ba6ec5ac15e3f1495f477d4ee357

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-trans.h
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-trans.h
@@ -473,6 +473,22 @@ struct iwl_trans_rxq_dma_data {
 	u64 ur_bd_cb;
 };
 
+/* maximal number of DRAM MAP entries supported by FW */
+#define IPC_DRAM_MAP_ENTRY_NUM_MAX 64
+
+/**
+ * struct iwl_pnvm_image - contains info about the parsed pnvm image
+ * @chunks: array of pointers to pnvm payloads and their sizes
+ * @n_chunks: the number of the pnvm payloads.
+ */
+struct iwl_pnvm_image {
+	struct {
+		const void *data;
+		u32 len;
+	} chunks[IPC_DRAM_MAP_ENTRY_NUM_MAX];
+	u32 n_chunks;
+};
+
 /**
  * struct iwl_trans_ops - transport specific operations
  *
@@ -633,7 +649,8 @@ struct iwl_trans_ops {
 						 void *sanitize_ctx);
 	void (*debugfs_cleanup)(struct iwl_trans *trans);
 	void (*sync_nmi)(struct iwl_trans *trans);
-	int (*set_pnvm)(struct iwl_trans *trans, const void *data, u32 len);
+	int (*set_pnvm)(struct iwl_trans *trans,
+			const struct iwl_pnvm_image *pnvm_data);
 	int (*set_reduce_power)(struct iwl_trans *trans,
 				const void *data, u32 len);
 
@@ -1584,10 +1601,10 @@ void iwl_trans_sync_nmi_with_addr(struct iwl_trans *trans, u32 inta_addr,
 				  u32 sw_err_bit);
 
 static inline int iwl_trans_set_pnvm(struct iwl_trans *trans,
-				     const void *data, u32 len)
+				     const struct iwl_pnvm_image *pnvm_data)
 {
 	if (trans->ops->set_pnvm) {
-		int ret = trans->ops->set_pnvm(trans, data, len);
+		int ret = trans->ops->set_pnvm(trans, pnvm_data);
 
 		if (ret)
 			return ret;
