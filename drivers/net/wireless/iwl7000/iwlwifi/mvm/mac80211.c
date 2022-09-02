36092b05e4f743b9b5a68ccc31de892261d220fd
Johannes Berg <johannes.berg@intel.com>
2022-09-02 09:35:35 +0200

    CHROMIUM: iwl7000: mvm: enable MLO support
    We don't yet have interface combinations advertising, but
    that really only affects AP mode, in which case we'll just
    fail to bring up more than one link. In client mode, now
    mac80211 will have the links but not activate them, so we
    can fairly safely enable this now.
    
    Change-Id: I78c0ce57bdd616834a1af45377979bf7f74b96ee
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 77a636e9209993bd198d572a467265b3854830ae

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
@@ -390,6 +390,11 @@ int iwl_mvm_mac_setup_register(struct iwl_mvm *mvm)
 	ieee80211_hw_set(hw, SUPPORTS_AMSDU_IN_AMPDU);
 #endif
 
+	/* Set this early since we need to have it for the check below */
+	if (mvm->mld_api_is_used &&
+	    mvm->trans->trans_cfg->device_family >= IWL_DEVICE_FAMILY_BZ)
+		hw->wiphy->flags |= WIPHY_FLAG_SUPPORTS_MLO;
+
 	/*
 	 * With MLD FW API, it tracks timing by itself,
 	 * no need for any timing from the host
