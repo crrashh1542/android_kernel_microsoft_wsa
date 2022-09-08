166d546cd7c32dd43ff659de2d23e2c161b725f1
Alon Giladi <alon.giladi@intel.com>
2022-09-08 16:23:58 +0300

    CHROMIUM: iwl7000: Add support for fragmented pnvm images
    Add support for fragmented pnvm images, depending on the FW capability.
    
    Signed-off-by: Alon Giladi <alon.giladi@intel.com>
    Change-Id: I0278312e7c3355b224cd870d4f8cf6578d12f03e
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 828820547efa8008f78707f1f9d8c79b11cb047c

--- drivers/net/wireless/iwl7000/iwlwifi/fw/pnvm.c
+++ drivers/net/wireless/iwl7000/iwlwifi/fw/pnvm.c
@@ -271,7 +271,8 @@ static u8 *iwl_get_pnvm_image(struct iwl_trans *trans_p, size_t *len)
 }
 
 int iwl_pnvm_load(struct iwl_trans *trans,
-		  struct iwl_notif_wait_data *notif_wait)
+		  struct iwl_notif_wait_data *notif_wait,
+		  const struct iwl_ucode_capabilities *capa)
 {
 	u8 *data;
 	size_t length;
@@ -303,7 +304,7 @@ int iwl_pnvm_load(struct iwl_trans *trans,
 			goto reduce_tables;
 		}
 
-		ret = iwl_trans_load_pnvm(trans, &pnvm_data);
+		ret = iwl_trans_load_pnvm(trans, &pnvm_data, capa);
 		/* data can be free only after we finish using pnvm_data */
 		kfree(data);
 		if (ret)
@@ -311,7 +312,7 @@ int iwl_pnvm_load(struct iwl_trans *trans,
 		IWL_INFO(trans, "loaded PNVM version %08x\n", pnvm_data.version);
 	}
 
-	iwl_trans_set_pnvm(trans);
+	iwl_trans_set_pnvm(trans, capa);
 
 reduce_tables:
 	/* now try to get the reduce power table, if not loaded yet */
