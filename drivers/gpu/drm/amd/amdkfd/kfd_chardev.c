2dc872e2379ed756e77bfa21e5671ae1e90f4388
Philip Yang <Philip.Yang@amd.com>
2022-09-07 22:38:00 -0400

    UPSTREAM: drm/amdkfd: Track unified memory when switching xnack mode
    Unified memory usage with xnack off is tracked to avoid oversubscribe
    system memory, with xnack on, we don't track unified memory usage to
    allow memory oversubscribe. When switching xnack mode from off to on,
    subsequent free ranges allocated with xnack off will not unreserve
    memory. When switching xnack mode from on to off, subsequent free ranges
    allocated with xnack on will unreserve memory. Both cases cause memory
    accounting unbalanced.
    
    When switching xnack mode from on to off, need reserve already allocated
    svm range memory. When switching xnack mode from off to on, need
    unreserve already allocated svm range memory.
    
    v6: Take prange lock to access range child list
    v5: Handle prange child ranges
    v4: Handle reservation memory failure
    v3: Handle switching xnack mode race with svm_range_deferred_list_work
    v2: Handle both switching xnack from on to off and from off to on cases
    
    Signed-off-by: Philip Yang <Philip.Yang@amd.com>
    Reviewed-by: Felix Kuehling <Felix.Kuehling@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 8a7c3ce108544334a9c7906f61d2701f433e44ee)
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    
    BUG=b:265055111
    TEST=Build test
    
    Backported using forklift.py
    
    Change-Id: I67d4757788094da9c59d32e4fcb6925490ec6379

--- drivers/gpu/drm/amd/amdkfd/kfd_chardev.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_chardev.c
@@ -1588,6 +1588,8 @@ static int kfd_ioctl_smi_events(struct file *filep,
 	return kfd_smi_event_open(pdd->dev, &args->anon_fd);
 }
 
+#if IS_ENABLED(CONFIG_HSA_AMD_SVM)
+
 static int kfd_ioctl_set_xnack_mode(struct file *filep,
 				    struct kfd_process *p, void *data)
 {
@@ -1598,22 +1600,29 @@ static int kfd_ioctl_set_xnack_mode(struct file *filep,
 	if (args->xnack_enabled >= 0) {
 		if (!list_empty(&p->pqm.queues)) {
 			pr_debug("Process has user queues running\n");
-			mutex_unlock(&p->mutex);
-			return -EBUSY;
+			r = -EBUSY;
+			goto out_unlock;
 		}
-		if (args->xnack_enabled && !kfd_process_xnack_mode(p, true))
+
+		if (p->xnack_enabled == args->xnack_enabled)
+			goto out_unlock;
+
+		if (args->xnack_enabled && !kfd_process_xnack_mode(p, true)) {
 			r = -EPERM;
-		else
-			p->xnack_enabled = args->xnack_enabled;
+			goto out_unlock;
+		}
+
+		r = svm_range_switch_xnack_reserve_mem(p, args->xnack_enabled);
 	} else {
 		args->xnack_enabled = p->xnack_enabled;
 	}
+
+out_unlock:
 	mutex_unlock(&p->mutex);
 
 	return r;
 }
 
-#if IS_ENABLED(CONFIG_HSA_AMD_SVM)
 static int kfd_ioctl_svm(struct file *filep, struct kfd_process *p, void *data)
 {
 	struct kfd_ioctl_svm_args *args = data;
@@ -1633,6 +1642,11 @@ static int kfd_ioctl_svm(struct file *filep, struct kfd_process *p, void *data)
 	return r;
 }
 #else
+static int kfd_ioctl_set_xnack_mode(struct file *filep,
+				    struct kfd_process *p, void *data)
+{
+	return -EPERM;
+}
 static int kfd_ioctl_svm(struct file *filep, struct kfd_process *p, void *data)
 {
 	return -EPERM;
