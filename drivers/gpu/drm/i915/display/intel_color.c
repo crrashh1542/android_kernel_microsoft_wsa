71e00758e2ef0565b1efb1b14c315e3086552faa
Ville Syrjälä <ville.syrjala@linux.intel.com>
2022-10-26 14:39:04 +0300

    UPSTREAM: drm/i915: Reject YCbCr output with degamma+gamma on pre-icl
    Since the pipe CSC sits between the degamma and gamma LUTs there
    is no way to make us it for RGB->YCbCr conversion when both LUTs
    are also active. Simply reject such combos.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221026113906.10551-10-ville.syrjala@linux.intel.com
    Reviewed-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    (cherry picked from commit 4c0119dd0a7728456a2394c1bc0aa612e1f2c3cb)
    
    BUG=b:272776823
    TEST=Toggle "Ambient colors" on hatch; no flickering observed
    
    Change-Id: I27f407eea45f1cf6cc7bd1d473ef19087136cd6d
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4445468
    Reviewed-by: Manasi Navare <navaremanasi@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/i915/display/intel_color.c
+++ drivers/gpu/drm/i915/display/intel_color.c
@@ -1571,7 +1571,14 @@ static int ivb_color_check(struct intel_crtc_state *crtc_state)
 	if (crtc_state->output_format != INTEL_OUTPUT_FORMAT_RGB &&
 	    crtc_state->hw.ctm) {
 		drm_dbg_kms(&i915->drm,
-			    "YCBCR and CTM together are not possible\n");
+			    "YCbCr and CTM together are not possible\n");
+		return -EINVAL;
+	}
+
+	if (crtc_state->output_format != INTEL_OUTPUT_FORMAT_RGB &&
+	    crtc_state->hw.degamma_lut && crtc_state->hw.gamma_lut) {
+		drm_dbg_kms(&i915->drm,
+			    "YCbCr and degamma+gamma together are not possible\n");
 		return -EINVAL;
 	}
 
@@ -1639,7 +1646,14 @@ static int glk_color_check(struct intel_crtc_state *crtc_state)
 	if (crtc_state->output_format != INTEL_OUTPUT_FORMAT_RGB &&
 	    crtc_state->hw.ctm) {
 		drm_dbg_kms(&i915->drm,
-			    "YCBCR and CTM together are not possible\n");
+			    "YCbCr and CTM together are not possible\n");
+		return -EINVAL;
+	}
+
+	if (crtc_state->output_format != INTEL_OUTPUT_FORMAT_RGB &&
+	    crtc_state->hw.degamma_lut && crtc_state->hw.gamma_lut) {
+		drm_dbg_kms(&i915->drm,
+			    "YCbCr and degamma+gamma together are not possible\n");
 		return -EINVAL;
 	}
 
