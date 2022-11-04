675b4fa09388811028b668cef22747175b6ef900
Youghandhar Chintala <quic_youghand@quicinc.com>
2022-11-04 14:24:03 +0530

    FROMGIT: wifi: ath11k: Trigger sta disconnect on hardware restart
    Currently after the hardware restart triggered from the driver, the
    station interface connection remains intact, since a disconnect trigger
    is not sent to userspace. This can lead to a problem in targets where
    the wifi mac sequence is added by the firmware.
    
    After the target restart, its wifi mac sequence number gets reset to
    zero. Hence AP to which our device is connected will receive frames with
    a  wifi mac sequence number jump to the past, thereby resulting in the
    AP dropping all these frames, until the frame arrives with a wifi mac
    sequence number which AP was expecting.
    
    To avoid such frame drops, its better to trigger a station disconnect
    upon target hardware restart which can be done with API
    ieee80211_reconfig_disconnect exposed to mac80211.
    
    The other targets are not affected by this change, since the hardware
    params flag is not set.
    
    Reported-by: kernel test robot <lkp@intel.com>
    
    Tested-on: WCN6750 hw1.0 AHB WLAN.MSL.1.0.1-00887-QCAMSLSWPLZ-1
    
    Signed-off-by: Youghandhar Chintala <quic_youghand@quicinc.com>
    Signed-off-by: Kalle Valo <quic_kvalo@quicinc.com>
    Link: https://lore.kernel.org/r/20221104085403.11025-1-quic_youghand@quicinc.com
    (cherry picked from commit a018750a2cceaf4427c4ee3d9ce3e83a171d5bd6
     https://git.kernel.org/pub/scm/linux/kernel/git/kvalo/ath.git ath-next)
    
    BUG=b:248457431
    TEST=emerge-herobrine chromeos-kernel-5_15; deploy and test basic WiFi
         functionalities on herobrine device.
    TEST=suite:wifi_matfunc results are consistent with lab results.
    
    Signed-off-by: Jun Yu <junyuu@chromium.org>
    Change-Id: I53797f4aa666a4c0797a91b48fa0000cf79eebec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4024240
    Reviewed-by: Kevin Lund <kglund@google.com>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Abhishek Kumar <kuabhs@chromium.org>

--- drivers/net/wireless/ath/ath11k/core.c
+++ drivers/net/wireless/ath/ath11k/core.c
@@ -198,6 +198,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.tcl_ring_retry = true,
 		.tx_ring_size = DP_TCL_DATA_RING_SIZE,
 		.smp2p_wow_exit = false,
+		.support_fw_mac_sequence = false,
 	},
 	{
 		.name = "qca6390 hw2.0",
@@ -282,6 +283,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.tcl_ring_retry = true,
 		.tx_ring_size = DP_TCL_DATA_RING_SIZE,
 		.smp2p_wow_exit = false,
+		.support_fw_mac_sequence = true,
 	},
 	{
 		.name = "qcn9074 hw1.0",
@@ -363,6 +365,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.tcl_ring_retry = true,
 		.tx_ring_size = DP_TCL_DATA_RING_SIZE,
 		.smp2p_wow_exit = false,
+		.support_fw_mac_sequence = false,
 	},
 	{
 		.name = "wcn6855 hw2.0",
@@ -447,6 +450,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.tcl_ring_retry = true,
 		.tx_ring_size = DP_TCL_DATA_RING_SIZE,
 		.smp2p_wow_exit = false,
+		.support_fw_mac_sequence = true,
 	},
 	{
 		.name = "wcn6855 hw2.1",
@@ -530,6 +534,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.tcl_ring_retry = true,
 		.tx_ring_size = DP_TCL_DATA_RING_SIZE,
 		.smp2p_wow_exit = false,
+		.support_fw_mac_sequence = true,
 	},
 	{
 		.name = "wcn6750 hw1.0",
@@ -610,6 +615,7 @@ static const struct ath11k_hw_params ath11k_hw_params[] = {
 		.tcl_ring_retry = false,
 		.tx_ring_size = DP_TCL_DATA_RING_SIZE_WCN6750,
 		.smp2p_wow_exit = true,
+		.support_fw_mac_sequence = true,
 	},
 };
 
