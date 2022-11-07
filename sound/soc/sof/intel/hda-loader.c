1344e497711eaf83a5f6a755a340ce64420afb26
Kai Vehmanen <kai.vehmanen@linux.intel.com>
2022-11-07 09:26:21 +0200

    BACKPORT: FROMLIST: ASoC: SOF: Intel: hda-loader: use small buffer for iccmax stream
    The data received via iccmax stream is not used anywhere, so no need to
    allocate a big DMA buffer for it. This is especially important as the
    allocation is done even in cases where reload of the firmware is skipped
    and execution happens directly from the firmware stored in IMR.
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    (am from https://patchwork.kernel.org/patch/13033951/)
    (also found at https://lore.kernel.org/all/20221107072621.28904-1-
    peter.ujfalusi@linux.intel.com/)
    
    Conflits:
     sound/soc/sof/intel/hda-loader.c
     -upstream no longer uses plat_data to access firmware struct
    
    BUG=b:249413861
    TEST=Test audio playback after system suspend and resume
    
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Signed-off-by: Vamshi Krishna Gopal <vamshi.krishna.gopal@intel.com>
    Change-Id: I568b91285be8a970750f07d172a66cc16a67426e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4015324
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Commit-Queue: Victor Ding <victording@chromium.org>
    Tested-by: Matt Lu <matt.lu@lcfc.corp-partner.google.com>
    Reviewed-by: Terry Cheong <htcheong@chromium.org>

--- sound/soc/sof/intel/hda-loader.c
+++ sound/soc/sof/intel/hda-loader.c
@@ -320,10 +320,8 @@ int hda_cl_copy_fw(struct snd_sof_dev *sdev, struct hdac_ext_stream *hext_stream
 
 int hda_dsp_cl_boot_firmware_iccmax(struct snd_sof_dev *sdev)
 {
-	struct snd_sof_pdata *plat_data = sdev->pdata;
 	struct hdac_ext_stream *iccmax_stream;
 	struct hdac_bus *bus = sof_to_bus(sdev);
-	struct firmware stripped_firmware;
 	struct snd_dma_buffer dmab_bdl;
 	int ret, ret1;
 	u8 original_gb;
@@ -331,15 +329,11 @@ int hda_dsp_cl_boot_firmware_iccmax(struct snd_sof_dev *sdev)
 	/* save the original LTRP guardband value */
 	original_gb = snd_hdac_chip_readb(bus, VS_LTRP) & HDA_VS_INTEL_LTRP_GB_MASK;
 
-	if (plat_data->fw->size <= plat_data->fw_offset) {
-		dev_err(sdev->dev, "error: firmware size must be greater than firmware offset\n");
-		return -EINVAL;
-	}
-
-	stripped_firmware.size = plat_data->fw->size - plat_data->fw_offset;
-
-	/* prepare capture stream for ICCMAX */
-	iccmax_stream = hda_cl_stream_prepare(sdev, HDA_CL_STREAM_FORMAT, stripped_firmware.size,
+	/*
+	 * Prepare capture stream for ICCMAX. We do not need to store
+	 * the data, so use a buffer of PAGE_SIZE for receiving.
+	 */
+	iccmax_stream = hda_cl_stream_prepare(sdev, HDA_CL_STREAM_FORMAT, PAGE_SIZE,
 					      &dmab_bdl, SNDRV_PCM_STREAM_CAPTURE);
 	if (IS_ERR(iccmax_stream)) {
 		dev_err(sdev->dev, "error: dma prepare for ICCMAX stream failed\n");
