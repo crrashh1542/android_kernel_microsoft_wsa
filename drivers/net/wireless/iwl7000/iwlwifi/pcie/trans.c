319c92ef81d90efb5ebfe6a78387a4eea8622ed8
Johannes Berg <johannes.berg@intel.com>
2022-11-11 10:54:07 +0100

    CHROMIUM: iwl7000: pcie: adjust Bz device timings
    The 100ms shouldn't be needed, only 10ms. However after
    reset we should have 10ms as well for these devices.
    
    Change-Id: Ifcfbccd5458bd9ebd496aa834284fb0facfcaaef
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 44adb427a7b512defd3458cef8a27904ac88b712

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
@@ -134,14 +134,17 @@ static int iwl_trans_pcie_sw_reset(struct iwl_trans *trans,
 				   bool retake_ownership)
 {
 	/* Reset entire device - do controller reset (results in SHRD_HW_RST) */
-	if (trans->trans_cfg->device_family >= IWL_DEVICE_FAMILY_BZ)
+	if (trans->trans_cfg->device_family >= IWL_DEVICE_FAMILY_BZ) {
 		iwl_set_bit(trans, CSR_GP_CNTRL,
 			    CSR_GP_CNTRL_REG_FLAG_SW_RESET);
-	else
+		usleep_range(10000 * CPTCFG_IWL_DELAY_FACTOR,
+			     20000 * CPTCFG_IWL_DELAY_FACTOR);
+	} else {
 		iwl_set_bit(trans, CSR_RESET,
 			    CSR_RESET_REG_FLAG_SW_RESET);
-	usleep_range(5000 * CPTCFG_IWL_DELAY_FACTOR,
-		     6000 * CPTCFG_IWL_DELAY_FACTOR);
+		usleep_range(5000 * CPTCFG_IWL_DELAY_FACTOR,
+			     6000 * CPTCFG_IWL_DELAY_FACTOR);
+	}
 
 	if (retake_ownership)
 		return iwl_pcie_prepare_card_hw(trans);
@@ -479,7 +482,8 @@ void iwl_pcie_apm_stop_master(struct iwl_trans *trans)
 				   CSR_GP_CNTRL_REG_FLAG_BUS_MASTER_DISABLE_STATUS,
 				   CSR_GP_CNTRL_REG_FLAG_BUS_MASTER_DISABLE_STATUS,
 				   100);
-		msleep(100);
+		usleep_range(10000 * CPTCFG_IWL_DELAY_FACTOR,
+			     20000 * CPTCFG_IWL_DELAY_FACTOR);
 	} else {
 		iwl_set_bit(trans, CSR_RESET, CSR_RESET_REG_FLAG_STOP_MASTER);
 
