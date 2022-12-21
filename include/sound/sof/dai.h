226f74ac3d25386f41698a7940472fe33c4c9527
V sujith kumar Reddy <vsujithkumar.reddy@amd.corp-partner.google.com>
2022-12-21 17:47:48 +0530

    Revert "UPSTREAM: ASoC: SOF: Adding amd HS functionality to the sof core"
    This reverts commit 950b3a3cfeca890de74a1578da24401238a82ff0.
    
    Reason for revert: Cherry-picking patch from upstream.
    
    BUG=b:237354782
    TEST=TESTED on skyrim
    
    Change-Id: Ia230845e717c55c8b8961c0251071b54843c3594
    Signed-off-by: V sujith kumar Reddy <vsujithkumar.reddy@amd.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4121898
    Tested-by: Baili Deng <bailideng@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Baili Deng <bailideng@google.com>

--- include/sound/sof/dai.h
+++ include/sound/sof/dai.h
@@ -84,7 +84,6 @@ enum sof_ipc_dai_type {
 	SOF_DAI_AMD_BT,			/**< AMD ACP BT*/
 	SOF_DAI_AMD_SP,			/**< AMD ACP SP */
 	SOF_DAI_AMD_DMIC,		/**< AMD ACP DMIC */
-	SOF_DAI_AMD_HS,			/**< Amd HS */
 	SOF_DAI_MEDIATEK_AFE,		/**< Mediatek AFE */
 };
 
@@ -113,7 +112,6 @@ struct sof_ipc_dai_config {
 		struct sof_ipc_dai_acp_params acpbt;
 		struct sof_ipc_dai_acp_params acpsp;
 		struct sof_ipc_dai_acpdmic_params acpdmic;
-		struct sof_ipc_dai_acp_params acphs;
 		struct sof_ipc_dai_mtk_afe_params afe;
 	};
 } __packed;
