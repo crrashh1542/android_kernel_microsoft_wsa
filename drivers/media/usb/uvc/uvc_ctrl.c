08e2a585ee0319bb28b7f3cddc0df6dc07f9f6ea
Yunke Cao <yunkec@chromium.org>
2023-01-05 17:40:38 +0900

    CHROMIUM: media: uvcvideo: disable a Realtek camera ROI
    The camera does not handle ROI max correctly.
    It is causing V4L2 compliance failure.
    
    BUG=b:262499795
    UPSTREAM-TASK=b:255969015
    TEST=Tested on primus
    Change-Id: If34a93cff385f303a0be6c4b9161e7690f0e9829
    Signed-off-by: Yunke Cao <yunkec@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4137993
    Reviewed-by: Ricardo Ribalda <ribalda@chromium.org>
    (cherry picked from commit c0327d2eeaeebaefa98ef2c71730a36aa2360dd1)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4149788
    Commit-Queue: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/usb/uvc/uvc_ctrl.c
+++ drivers/media/usb/uvc/uvc_ctrl.c
@@ -2441,6 +2441,8 @@ static void uvc_ctrl_prune_entity(struct uvc_device *dev,
 		{ { USB_DEVICE(0x0bda, 0x5539) }, 21 },
 		/* Realtek Semiconductor Corp. Integrated_Webcam_HD */
 		{ { USB_DEVICE(0x0bda, 0x565c) }, 21 },
+		/* Realtek Semiconductor Corp. Integrated_Webcam_HD */
+		{ { USB_DEVICE(0x0bda, 0x5676) }, 21 },
 		/* Lenovo Integrated Camera */
 		{ { USB_DEVICE(0x30c9, 0x0093) }, 21 },
 	};
