1c73181e5855b3e406d0ecf411c85197bb73a68b
Johannes Berg <johannes.berg@intel.com>
2023-02-07 22:22:56 +0100

    CHROMIUM: iwl7000: mvm: use get_random_u32_inclusive()
    This was already upstream in Jason's work, but use it here so
    the driver can compile on 6.2 kernels.
    
    Change-Id: I9646f67529486a25d0e132cf43ec01e8e862679d
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 2c7480864f9234f358f228c25678eaee5f7b4a7c

--- drivers/net/wireless/iwl7000/hdrs/iwl-chrome.h
+++ drivers/net/wireless/iwl7000/hdrs/iwl-chrome.h
@@ -629,3 +629,18 @@ kthread_complete_and_exit(struct completion *c, long ret)
 	complete_and_exit(c, ret);
 }
 #endif /* <v5.17 */
+
+#if LINUX_VERSION_IS_LESS(6,1,0)
+static inline u32 get_random_u32_below(u32 ceil)
+{
+	return prandom_u32_max(ceil);
+}
+
+static inline u32 get_random_u32_inclusive(u32 floor, u32 ceil)
+{
+	BUILD_BUG_ON_MSG(__builtin_constant_p(floor) && __builtin_constant_p(ceil) &&
+			 (floor > ceil || ceil - floor == U32_MAX),
+			 "get_random_u32_inclusive() must take floor <= ceil");
+	return floor + get_random_u32_below(ceil - floor + 1);
+}
+#endif
