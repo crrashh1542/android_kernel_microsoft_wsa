bd263bcfe6dde54fdb43535f4de1bcb2fbb800fd
Alon Giladi <alon.giladi@intel.com>
2023-02-07 11:55:42 +0200

    CHROMIUM: iwl7000: mac80211: drop unprotected robust mgmt before 4-way-HS
    When MFP is used, drop unprotected robust management frames also
    before the 4-way handshake has been completed, i.e. no key has
    been installed yet.
    
    Change-Id: Ife369dbb61c87e311ce15739d5b2b4763bfdfbae
    Signed-off-by: Alon Giladi <alon.giladi@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: af42524d0a80380161d7bdf802cad287a41790cf

--- drivers/net/wireless/iwl7000/mac80211/rx.c
+++ drivers/net/wireless/iwl7000/mac80211/rx.c
@@ -2418,8 +2418,7 @@ static int ieee80211_drop_unencrypted_mgmt(struct ieee80211_rx_data *rx)
 
 	if (rx->sta && test_sta_flag(rx->sta, WLAN_STA_MFP)) {
 		if (unlikely(!ieee80211_has_protected(fc) &&
-			     ieee80211_is_unicast_robust_mgmt_frame(rx->skb) &&
-			     rx->key)) {
+			     ieee80211_is_unicast_robust_mgmt_frame(rx->skb))) {
 			if (ieee80211_is_deauth(fc) ||
 			    ieee80211_is_disassoc(fc))
 				cfg80211_rx_unprot_mlme_mgmt(rx->sdata->dev,
