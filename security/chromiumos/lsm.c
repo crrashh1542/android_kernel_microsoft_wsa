aec102902e1a8ababf8c46ae2863554ad89fea40
Brian Geffon <bgeffon@chromium.org>
2023-03-08 13:42:05 -0500

    CHROMIUM: security: Add a DM LSM
    This LSM will prevent mknod on hiberimage. This will allow us to
    unlink it from the file system and prevent it from being recreated.
    
        # dmsetup ls
        hiberimage_integrity    (254:9)
        ...
        hiberintegrity  (254:6)
        ...
        hiberimage      (254:10)
    
        # rm /dev/dm-10
        # rm /dev/mapper/hiberimage
    
        # ls -la /dev/dm-10
        ls: cannot access '/dev/dm-10': No such file or directory
    
        # dmsetup mknodes hiberimage
        /dev/mapper/hiberimage: mknod for hiberimage failed: Operation not permitted
    
        # mknod hiberimage b 254 10
        mknod: hiberimage: Operation not permitted
    
    This change squashing in 3 changes from 5.10:
    
      aa7861c4 - CHROMIUM: security: Add a DM LSM
      aacf4827 - CHROMIUM: security: DM LSM to check uuid instead of name
      58e25d86 - CHROMIUM: security: Block open on dm_locked devices
    
    Signed-off-by: Brian Geffon <bgeffon@chromium.org>
    
    UPSTREAM-TASK=b:273250069
    TEST=Brya-hibernate manually
    BUG=b:266841303
    
    Change-Id: I6ab535990e7c0f88f3312e49a14f35be3bd45983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4594816
    Tested-by: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Commit-Queue: Brian Geffon <bgeffon@chromium.org>

--- security/chromiumos/lsm.c
+++ security/chromiumos/lsm.c
@@ -23,6 +23,7 @@
 #include <linux/audit.h>
 #include <linux/binfmts.h>
 #include <linux/cred.h>
+#include <linux/device-mapper.h>
 #include <linux/fs.h>
 #include <linux/fs_parser.h>
 #include <linux/fs_struct.h>
@@ -184,11 +185,35 @@ static int chromiumos_security_inode_follow_link(struct dentry *dentry,
 	return policy == CHROMIUMOS_INODE_POLICY_BLOCK ? -EACCES : 0;
 }
 
+#define DM_LOCKED_PREFIX "dm_locked-"
+static bool chromiumos_locked_down_dm_device(dev_t dev)
+{
+	bool ret = false;
+	struct mapped_device *md;
+	char dm_uuid[DM_UUID_LEN]; /* 129 bytes */
+
+	md = dm_get_md(dev);
+	if (!md)
+		return false;
+
+	if (!dm_copy_name_and_uuid(md, NULL, dm_uuid) &&
+			str_has_prefix(dm_uuid, DM_LOCKED_PREFIX))
+			ret = true;
+
+	dm_put(md);
+	return ret;
+}
+
 static int chromiumos_security_file_open(struct file *file)
 {
 	static char accessed_path[PATH_MAX];
 	enum chromiumos_inode_security_policy policy;
 	struct dentry *dentry = file->f_path.dentry;
+
+	/* if it's a dm block device that's locked down return -EPERM */
+	if (S_ISBLK(file->f_inode->i_mode) &&
+			chromiumos_locked_down_dm_device(file->f_inode->i_rdev))
+		return -EPERM;
 
 	/* Returns 0 if file is not a FIFO */
 	if (!S_ISFIFO(file->f_inode->i_mode))
@@ -287,6 +312,47 @@ static int chromiumos_locked_down(enum lockdown_reason what)
 	return 0;
 }
 
+/*
+ * This specific function will prevent mknod of 3 specific device mapper devices.
+ * If an attempt is made to mknod hiberimage, hiberintegrity, or hiberimage_integrity it will
+ * fail with a -EPERM.
+ *
+ * When device mapper first creates a device using dmsetup the node created is a dm-N node, this
+ * happens before a table has been made live. Once the table has been made live a symbolic link is
+ * created in /dev/mapper/DM_NAME pointing to the dm-N node that was previously created.
+ * This method specifically queries the name of the dm device, that is, it's a no-op if the device
+ * mapper device has no table (and thus no name). Once a table has been established if the name of the
+ * device is one of the three restricted ones any future mknod will be rejected with -EPERM.
+ *
+ * The typical flow would be: establish the dm-crypt/dm-integrity hibernate volumes. Once they are
+ * created they are opened by the kernel using the /dev/snapshot set-device ioctl. When the kernel
+ * has it opened it will then be unlinked from the file system and once it has been unlink since
+ * we're blocking mknod there will be no way to recreate the node.
+ *
+ */
+static int chromiumos_security_dm_mknod(struct dentry *dentry, umode_t mode, dev_t dev)
+{
+	/* if it's a dm block device that's locked down, return -EPERM */
+	if (S_ISBLK(mode) && chromiumos_locked_down_dm_device(dev))
+		return -EPERM;
+
+	return 0;
+}
+
+static int chromiumos_security_path_mknod(const struct path *const dir,
+			   struct dentry *const dentry, const umode_t mode,
+			   const unsigned int dev)
+{
+	return chromiumos_security_dm_mknod(dentry, mode, new_decode_dev(dev));
+}
+
+static int chromiumos_security_inode_mknod(struct inode *dir, struct dentry *dentry,
+		umode_t mode, dev_t dev)
+{
+	return chromiumos_security_dm_mknod(dentry, mode, dev);
+}
+
+
 static struct security_hook_list chromiumos_security_hooks[] = {
 	LSM_HOOK_INIT(sb_mount, chromiumos_security_sb_mount),
 	LSM_HOOK_INIT(inode_follow_link, chromiumos_security_inode_follow_link),
@@ -294,6 +360,8 @@ static struct security_hook_list chromiumos_security_hooks[] = {
 	LSM_HOOK_INIT(sb_eat_lsm_opts, chromiumos_sb_eat_lsm_opts),
 	LSM_HOOK_INIT(bprm_creds_for_exec, chromiumos_bprm_creds_for_exec),
 	LSM_HOOK_INIT(locked_down, chromiumos_locked_down),
+	LSM_HOOK_INIT(path_mknod, chromiumos_security_path_mknod),
+	LSM_HOOK_INIT(inode_mknod, chromiumos_security_inode_mknod),
 };
 
 static int __init chromiumos_security_init(void)
