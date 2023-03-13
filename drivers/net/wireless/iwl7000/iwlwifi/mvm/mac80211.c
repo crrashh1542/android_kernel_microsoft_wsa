13994dc29b2da3e20b87fa9a4b5f0db73caf479d
Johannes Berg <johannes.berg@intel.com>
2023-03-13 12:02:58 +0100

    CHROMIUM: iwl7000: mvm: support flush on AP interfaces
    Support TX flush on AP interfaces so that we will do a
    proper flush for frames on the queue before keys are
    removed.
    
    Change-Id: Ia8381bd4f7d47592e74387c564739798a01c4049
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: 1d76b99d00816a91bc87c619b39d4bc87a9974f7

--- drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mvm/mac80211.c
@@ -5929,9 +5929,6 @@ void iwl_mvm_mac_flush(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 		return;
 	}
 
-	if (vif->type != NL80211_IFTYPE_STATION)
-		return;
-
 	/* Make sure we're done with the deferred traffic before flushing */
 	flush_work(&mvm->add_stream_wk);
 
@@ -5954,9 +5951,6 @@ void iwl_mvm_mac_flush(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 				continue;
 			ap_sta_done = true;
 		}
-
-		/* make sure only TDLS peers or the AP are flushed */
-		WARN_ON_ONCE(sta != mvmvif->ap_sta && !sta->tdls);
 
 		if (drop) {
 			if (iwl_mvm_flush_sta(mvm, mvmsta, false))
