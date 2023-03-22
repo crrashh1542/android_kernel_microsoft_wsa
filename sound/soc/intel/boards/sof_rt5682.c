bb99b1e522ed5a9a566e229b238adb6a60ce7519
Uday M Bhat <uday.m.bhat@intel.com>
2023-03-22 09:49:16 +0200

    UPSTREAM: ASoC: Intel: sof_rt5682: Update BT offload config for MTL RVP
    For MTL RVP, SSP2 is used for BT offload. This is enabled
    in the sof_rt5682_quirk_table
    
    Signed-off-by: Uday M Bhat <uday.m.bhat@intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Link: https://lore.kernel.org/r/20230322074916.23225-3-peter.ujfalusi@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit f19f24d47900656962cafa51426660c8aa32744d)
    
    BUG=b:315885644
    TEST=Success to build kernel
    
    Change-Id: I791e602bc5bd8887a2e50904b7a35e646d7a5619
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5348536
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Terry Cheong <htcheong@chromium.org>

--- sound/soc/intel/boards/sof_rt5682.c
+++ sound/soc/intel/boards/sof_rt5682.c
@@ -1279,7 +1279,9 @@ static const struct platform_device_id board_ids[] = {
 		.driver_data = (kernel_ulong_t)(SOF_RT5682_MCLK_EN |
 					SOF_RT5682_SSP_CODEC(0) |
 					SOF_RT5682_SSP_AMP(1) |
-					SOF_RT5682_NUM_HDMIDEV(4)),
+					SOF_RT5682_NUM_HDMIDEV(4) |
+					SOF_BT_OFFLOAD_SSP(2) |
+					SOF_SSP_BT_OFFLOAD_PRESENT),
 	},
 	{
 		.name = "mtl_mx98360_rt5682",
