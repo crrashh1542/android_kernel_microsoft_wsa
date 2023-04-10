0228d8b34eefec6fc70c9867f0e2f2098d2a7945
Chang S. Bae <chang.seok.bae@intel.com>
2023-04-10 15:59:33 -0700

    FROMLIST: x86/cpu: Add a configuration and command line option for Key Locker
    Add CONFIG_X86_KEYLOCKER to gate whether Key Locker is initialized at boot.
    The option is selected by the Key Locker cipher module CRYPTO_AES_KL (to be
    added in a later patch).
    
    Add a new command line option "nokeylocker" to optionally override the
    default CONFIG_X86_KEYLOCKER=y behavior.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: linux-doc@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    
    (am from https://patchwork.kernel.org/patch/13206825)
    
    BUG=b:223655857
    TEST=Check dmesg keylocker enabled
    TEST=Boot, check aeskl(xts) in /proc/crypto
    TEST=Basic read/write sanity on dm-crypt device with aeskl
    TEST=Login flow with Keylocker enabled on user cryptohomes
    
    Change-Id: I5b51a8c901248fdd914b6d311e2aa68e339ec3eb
    Signed-off-by: George D Sworo <george.d.sworo@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4456178
    Commit-Queue: Sarthak Kukreti <sarthakkukreti@google.com>
    Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Tested-by: Sarthak Kukreti <sarthakkukreti@google.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -358,6 +358,22 @@ out:
 	cr4_clear_bits(X86_CR4_UMIP);
 }
 
+
+static __init int x86_nokeylocker_setup(char *arg)
+{
+	/* Expect an exact match without trailing characters. */
+	if (strlen(arg))
+		return 0;
+
+	if (!cpu_feature_enabled(X86_FEATURE_KEYLOCKER))
+		return 1;
+
+	setup_clear_cpu_cap(X86_FEATURE_KEYLOCKER);
+	pr_info("x86/keylocker: Disabled by kernel command line.\n");
+	return 1;
+}
+__setup("nokeylocker", x86_nokeylocker_setup);
+
 /* These bits should not change their value after CPU init is finished. */
 static const unsigned long cr4_pinned_mask =
 	X86_CR4_SMEP | X86_CR4_SMAP | X86_CR4_UMIP | X86_CR4_FSGSBASE;
