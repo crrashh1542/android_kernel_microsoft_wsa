62ca0245335e13f152822a8adeae1d8dbbdd551a
Chang S. Bae <chang.seok.bae@intel.com>
2023-04-10 15:59:29 -0700

    BACKPORT: FROMLIST: x86/msr-index: Add MSRs for Key Locker internal wrapping key
    The CPU state that contains the internal wrapping key is in the same power
    domain as the cache. So any sleep state that would invalidate the cache
    (like S3) also invalidates the state of the wrapping key.
    
    But, since the state is inaccessible to software, it needs a special
    mechanism to save and restore the key during deep sleep.
    
    A set of new MSRs are provided as an abstract interface to save and restore
    the wrapping key, and to check the key status.
    
    The wrapping key is saved in a platform-scoped state of non-volatile media.
    The backup itself and its path from the CPU are encrypted and integrity
    protected.
    
    But this storage's non-volatility is not architecturally guaranteed across
    off states, such as S5 and G3.
    
    The MSRs will be used to back up the key for S3/4 sleep states. Then the
    kernel code writes one of them to restore the key in each CPU state.
    
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: x86@kernel.org
    Cc: linux-kernel@vger.kernel.org
    
    (am from https://patchwork.kernel.org/patch/13206821)
    
    BUG=b:223655857
    TEST=Check dmesg keylocker enabled
    TEST=Boot, check aeskl(xts) in /proc/crypto
    TEST=Basic read/write sanity on dm-crypt device with aeskl
    TEST=Login flow with Keylocker enabled on user cryptohomes
    
    Change-Id: Ib6e17b3aa1b2c0866fb0e9677820edc753c3818b
    Signed-off-by: George D Sworo <george.d.sworo@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4456174
    Commit-Queue: Sarthak Kukreti <sarthakkukreti@google.com>
    Reviewed-by: Sarthak Kukreti <sarthakkukreti@google.com>
    Tested-by: Sarthak Kukreti <sarthakkukreti@google.com>
    Reviewed-by: Charishma Gairuboyina <charishma1.gairuboyina@intel.corp-partner.google.com>

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -1010,4 +1010,10 @@
 #define MSR_IA32_HW_FEEDBACK_PTR        0x17d0
 #define MSR_IA32_HW_FEEDBACK_CONFIG     0x17d1
 
+/* MSRs for managing an internal wrapping key for Key Locker. */
+#define MSR_IA32_IWKEY_COPY_STATUS		0x00000990
+#define MSR_IA32_IWKEY_BACKUP_STATUS		0x00000991
+#define MSR_IA32_BACKUP_IWKEY_TO_PLATFORM	0x00000d91
+#define MSR_IA32_COPY_IWKEY_TO_LOCAL		0x00000d92
+
 #endif /* _ASM_X86_MSR_INDEX_H */
