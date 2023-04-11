18664f937a273b08fe839e887cbfd2f18eb6c96c
Paul Menzel <pmenzel@molgen.mpg.de>
2023-04-11 20:31:44 +0200

    BACKPORT: ACPI: resource: Skip IRQ override on ASUS ExpertBook B1502CBA
    Like the ASUS ExpertBook B2502CBA and various ASUS Vivobook laptops, the
    ASUS ExpertBook B1502CBA has an ACPI DSDT table that describes IRQ 1 as
    ActiveLow while the kernel overrides it to Edge_High.
    
        $ sudo dmesg | grep DMI
        DMI: ASUSTeK COMPUTER INC. ASUS EXPERTBOOK
        B1502CBA_B1502CBA/B1502CBA,
        BIOS B1502CBA.300 01/18/2023
        $ grep -A 40 PS2K dsdt.dsl | grep IRQ -A 1
                        IRQ (Level, ActiveLow, Exclusive, )
                            {1}
    
    This prevents the keyboard from working. To fix this issue, add this laptop
    to the skip_override_table so that the kernel does not override IRQ 1.
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=217323
    Signed-off-by: Paul Menzel <pmenzel@molgen.mpg.de>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit 05cda427126f30ce3fc8ffd82fd6f5196398d502)
    
    Conflicts:
       drivers/acpi/resource.c
    
    BUG=b:309985669
    TEST=Deploy reven kernel to Asus ExpertBook B1502CBA and verify the
         keyboard works
    
    Change-Id: I0200fd4b37c8c22707c0276f653791ddf2f49576
    Signed-off-by: Jonathan Denose <jdenose@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5261513
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    Commit-Queue: Dmitry Torokhov <dtor@chromium.org>

--- drivers/acpi/resource.c
+++ drivers/acpi/resource.c
@@ -425,6 +425,13 @@ static const struct dmi_system_id asus_laptop[] = {
 			DMI_MATCH(DMI_BOARD_NAME, "K3502ZA"),
 		},
 	},
+	{
+		.ident = "Asus ExpertBook B1502CBA",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_BOARD_NAME, "B1502CBA"),
+		},
+	},
 	{ }
 };
 
