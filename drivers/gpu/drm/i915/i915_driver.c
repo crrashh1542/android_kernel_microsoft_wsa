44b5553e4a52067966aacc38b1dfea98320ef4dd
Rob Clark <robdclark@chromium.org>
2023-04-12 15:42:56 -0700

    BACKPORT: FROMLIST: drm/i915: Switch to fdinfo helper
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    (am from https://patchwork.kernel.org/patch/13209611/)
    (also found at https://lore.kernel.org/r/20230412224311.23511-5-robdclark@gmail.com)
    
    Conflicts:
       drivers/gpu/drm/i915/i915_drm_client.c
    
    BACKPORT NOTE: Will be reverted and replaced with intel's upstream patch
    once that exists.
    
    BUG=b:278115755
    TEST=Boot wormdingler, check /proc/$pid/fdinfo/$fd
    UPSTREAM-TASK=b:284166503
    
    Change-Id: I04773095ad6a1fed7d0131dad3ab7828f84675fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4562527
    Commit-Queue: Rob Clark <robdclark@chromium.org>
    Auto-Submit: Rob Clark <robdclark@chromium.org>
    Tested-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- drivers/gpu/drm/i915/i915_driver.c
+++ drivers/gpu/drm/i915/i915_driver.c
@@ -1821,7 +1821,7 @@ static const struct file_operations i915_driver_fops = {
 	.compat_ioctl = i915_ioc32_compat_ioctl,
 	.llseek = noop_llseek,
 #ifdef CONFIG_PROC_FS
-	.show_fdinfo = i915_drm_client_fdinfo,
+	.show_fdinfo = drm_show_fdinfo,
 #endif
 };
 
@@ -1923,6 +1923,7 @@ static const struct drm_driver i915_drm_driver = {
 	.open = i915_driver_open,
 	.lastclose = i915_driver_lastclose,
 	.postclose = i915_driver_postclose,
+	.show_fdinfo = i915_drm_client_fdinfo,
 
 	.prime_handle_to_fd = drm_gem_prime_handle_to_fd,
 	.prime_fd_to_handle = drm_gem_prime_fd_to_handle,
