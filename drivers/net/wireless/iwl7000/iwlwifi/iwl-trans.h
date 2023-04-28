05e723e0a1782433127e05cdeb4ffdda51705dde
Johannes Berg <johannes.berg@intel.com>
2023-04-28 14:44:52 +0200

    CHROMIUM: iwl7000: remove spurious kernel-doc
    This snuck in in a previous patch after the struct member
    had been deemed unnecessary. Remove it.
    
    Change-Id: I394fd3a94da539aea5569c416216d3dcf06388e6
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: b6f027df47fefe0af958cff574673ac97474d523

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-trans.h
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-trans.h
@@ -756,7 +756,6 @@ struct iwl_dram_data {
  * struct iwl_dram_regions - DRAM regions container structure
  * @drams: array of several DRAM areas that contains the
  *	pnvm/power reduction table payloads.
- * @total_len: length of the entire payload, all chunks combined
  * @n_regions: number of DRAM regions that were allocated
  * @prph_scratch_mem_desc: points to a structure allocated in dram,
  *	designed to show FW where all the payloads are.
