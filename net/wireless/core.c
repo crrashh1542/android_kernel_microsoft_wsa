83ffd18d74b928f6c76307aaef41a87c7035b34a
Johannes Berg <johannes.berg@intel.com>
2023-06-06 14:49:20 +0200

    FROMGIT: wifi: cfg80211: hold wiphy lock in auto-disconnect
    Most code paths in cfg80211 already hold the wiphy lock,
    mostly by virtue of being called from nl80211, so make
    the auto-disconnect worker also hold it, aligning the
    locking promises between different parts of cfg80211.
    
    BUG=b:288121907
    TEST=check build
    
    (cherry picked from commit e9da6df7492a981b071bafd169fb4c35b45f5ebf
     git://git.kernel.org/pub/scm/linux/kernel/git/wireless/wireless-next.git main)
    
    Change-Id: Iabdd54eff8250664cfae041c5412ae25ef0f0c61
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4640001
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Tested-by: David Ruth <druth@chromium.org>
    Reviewed-by: David Ruth <druth@chromium.org>
    Commit-Queue: David Ruth <druth@chromium.org>

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -1148,10 +1148,6 @@ static void _cfg80211_unregister_wdev(struct wireless_dev *wdev,
 	kfree_sensitive(wdev->wext.keys);
 	wdev->wext.keys = NULL;
 #endif
-	/* only initialized if we have a netdev */
-	if (wdev->netdev)
-		flush_work(&wdev->disconnect_wk);
-
 	cfg80211_cqm_config_free(wdev);
 
 	/*
@@ -1411,6 +1407,8 @@ static int cfg80211_netdev_notifier_call(struct notifier_block *nb,
 		wiphy_lock(&rdev->wiphy);
 		cfg80211_leave(rdev, wdev);
 		wiphy_unlock(&rdev->wiphy);
+		/* since we just did cfg80211_leave() nothing to do there */
+		cancel_work_sync(&wdev->disconnect_wk);
 		break;
 	case NETDEV_DOWN:
 		wiphy_lock(&rdev->wiphy);
