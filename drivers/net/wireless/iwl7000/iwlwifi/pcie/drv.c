599dfc64152db8ca60de7e7b8a818598eb156a53
Ilan Peer <ilan.peer@intel.com>
2023-08-08 11:01:32 +0300

    CHROMIUM: iwl7000: Fix 160 MHz support
    If a matching device was found during PCI probing, set the
    'no_160' fields only if the device information explicitly stated
    the 160 MHz operation is not supported.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Change-Id: Ia4487657af2d4239150807086a74c9645fcc3d61
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    iwl7000-tree: df56d39998a4720468ce3aba412eaa0dc2060a28

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/drv.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/drv.c
@@ -1202,7 +1202,7 @@ static int iwl_pci_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	if (dev_info) {
 		iwl_trans->cfg = dev_info->cfg;
 		iwl_trans->name = dev_info->name;
-		iwl_trans->no_160 = IWL_SUBDEVICE_NO_160(pdev->subsystem_device);
+		iwl_trans->no_160 = dev_info->no_160 == IWL_CFG_NO_160;
 	}
 
 #if IS_ENABLED(CPTCFG_IWLMVM)
