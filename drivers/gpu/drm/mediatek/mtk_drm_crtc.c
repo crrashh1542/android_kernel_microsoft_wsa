658e660ffe088f8366500aabc2cf788d28b80a08
Jason-JH.Lin <jason-jh.lin@mediatek.com>
2023-08-22 21:26:46 +0800

    FROMLIST: drm/mediatek: Add spinlock for setting vblank event in atomic_begin
    Add spinlock protection to avoid race condition on vblank event
    between mtk_drm_crtc_atomic_begin() and mtk_drm_finish_page_flip().
    
    Fixes: 119f5173628a ("drm/mediatek: Add DRM Driver for Mediatek SoC MT8173.")
    Change-Id: Ieead671fe3f4a2741ac4f671ee59a0b099004c07
    Signed-off-by: Jason-JH.Lin <jason-jh.lin@mediatek.com>
    
    ---
    
    (am from https://patchwork.kernel.org/project/linux-mediatek/patch/20230822132646.9811-1-jason-jh.lin@mediatek.com/)
    
    BUG=b:294164119
    TEST=emerge-cherry chromeos-kernel-5_10 and boot DUT
    UPSTREAM-TASK=b:222031414
    
    Signed-off-by: Jason-JH.Lin <jason-jh.lin@mediatek.com>
    Change-Id: Ifc8af9c06e7d16b9c6f82e4acb687508aa78215b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4825227
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Tested-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Commit-Queue: Hsin-Yi Wang <hsinyi@chromium.org>

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.c
@@ -739,6 +739,9 @@ static void mtk_drm_crtc_atomic_begin(struct drm_crtc *crtc,
 									  crtc);
 	struct mtk_crtc_state *mtk_crtc_state = to_mtk_crtc_state(crtc_state);
 	struct mtk_drm_crtc *mtk_crtc = to_mtk_crtc(crtc);
+	unsigned long flags;
+
+	spin_lock_irqsave(&crtc->dev->event_lock, flags);
 
 	if (mtk_crtc->event && mtk_crtc_state->base.event)
 		DRM_ERROR("new event while there is still a pending event\n");
@@ -754,6 +757,8 @@ static void mtk_drm_crtc_atomic_begin(struct drm_crtc *crtc,
 				 __func__,
 				 mtk_crtc->event, mtk_crtc, crtc);
 	}
+
+	spin_unlock_irqrestore(&crtc->dev->event_lock, flags);
 }
 
 static void mtk_drm_crtc_atomic_flush(struct drm_crtc *crtc,
