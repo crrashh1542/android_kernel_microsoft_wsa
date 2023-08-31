d11a0913b4ef9cdfaab8b3fa061248268a86d14f
Ying Hsu <yinghsu@chromium.org>
2023-08-31 12:11:16 +0000

    Revert "FROMGIT: Bluetooth: hci_conn: Consolidate code for aborting connections"
    This reverts commit 281446193d86e4fcb2eafd00d42c4f8c49a54cdd.
    
    Reason for revert: BUG: sleeping function called from invalid context in hci_cmd_sync_queue 
    
    Original change's description:
    > FROMGIT: Bluetooth: hci_conn: Consolidate code for aborting connections
    >
    > This consolidates code for aborting connections using
    > hci_cmd_sync_queue so it is synchronized with other threads, but
    > because of the fact that some commands may block the cmd_sync_queue
    > while waiting specific events this attempt to cancel those requests by
    > using hci_cmd_sync_cancel.
    >
    > Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    > (cherry picked from commit a13f316e90fdb1fb6df6582e845aa9b3270f3581
    >  git://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/ master)
    >
    > BUG=b:204408624
    > TEST=run BluetoothAdapterLEHealth on brya 5.15
    >
    > Change-Id: I0bf9d1a19033ec2e5e77a8935919db23e63eae27
    > Signed-off-by: Yun-Hao Chung <howardchung@google.com>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4778361
    > Tested-by: Yun-Hao Chung <howardchung@chromium.org>
    > Commit-Queue: Yun-Hao Chung <howardchung@chromium.org>
    > Reviewed-by: Sean Paul <sean@poorly.run>
    > Reviewed-by: Archie Pusaka <apusaka@chromium.org>
    
    BUG=b:204408624
    BUG=b:296256880
    
    Change-Id: I283075e0d478f0a9a295a0bbbe2b2e28ae20b6cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4830632
    Commit-Queue: Alain Michaud <alainm@chromium.org>
    Auto-Submit: Ying Hsu <yinghsu@chromium.org>
    Reviewed-by: Alain Michaud <alainm@chromium.org>
    Commit-Queue: Ying Hsu <yinghsu@chromium.org>
    Tested-by: Ying Hsu <yinghsu@chromium.org>

--- include/net/bluetooth/hci_core.h
+++ include/net/bluetooth/hci_core.h
@@ -718,7 +718,6 @@ struct hci_conn {
 	unsigned long	flags;
 
 	enum conn_reasons conn_reason;
-	__u8		abort_reason;
 
 	__u32		clock;
 	__u16		clock_accuracy;
@@ -738,6 +737,7 @@ struct hci_conn {
 	struct delayed_work auto_accept_work;
 	struct delayed_work idle_work;
 	struct delayed_work le_conn_timeout;
+	struct work_struct  le_scan_cleanup;
 
 	struct device	dev;
 	struct dentry	*debugfs;
