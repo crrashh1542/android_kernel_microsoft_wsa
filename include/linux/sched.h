eeda0d41945da6772305c0cef2d4158c3fab07f7
Vineeth Pillai (Google) <vineeth@bitbyteword.org>
2023-10-04 19:43:51 -0400

    CHROMIUM: sched/core: boost/unboost in guest scheduler
    RT or higher priority tasks in guest is considered a critical workload
    and guest scheduler can request boost/unboost on a task switch and/or a
    task wakeup. Also share the preempt status of guest vcpu with the host
    so that host can take decision on boot/unboost.
    
    We use the function equivalent of preempt_count_{add,sub} functions
    defined via CONFIG_TRACE_PREEMPT_TOGGLE to track the preemption state.
    Another option is to update the preempt_count_{add,sub} macros, but
    it will be more code churn and complex.
    
    Boost request is lazy, but unboost request is synchronous.
    
    Detect the feature in guest from cpuid flags and use the MSR to pass the
    GPA of memory location for sharing scheduling information.
    
    UPSTREAM-TASK=b:303645537
    BUG=b:262267726
    TEST=boot
    
    Co-developed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Vineeth Pillai (Google) <vineeth@bitbyteword.org>
    
    Change-Id: I1df908aca326dab15e619ba87c348179e45f517d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5253697
    Reviewed-by: Joel Fernandes <joelaf@google.com>
    Tested-by: Vineeth Pillai <vineethrp@google.com>
    Commit-Queue: Vineeth Pillai <vineethrp@google.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2363,4 +2363,37 @@ static inline void sched_core_free(struct task_struct *tsk) { }
 static inline void sched_core_fork(struct task_struct *p) { }
 #endif
 
+#ifdef CONFIG_PARAVIRT_SCHED
+DECLARE_STATIC_KEY_FALSE(__pv_sched_enabled);
+
+extern unsigned long pv_sched_pa(void);
+
+static inline bool pv_sched_enabled(void)
+{
+	return static_branch_unlikely(&__pv_sched_enabled);
+}
+
+static inline void pv_sched_enable(void)
+{
+	static_branch_enable(&__pv_sched_enabled);
+}
+
+extern void pv_sched_vcpu_update(int policy, int prio, int nice, bool lazy);
+extern void pv_sched_vcpu_kerncs_unboost(int boost_type, bool lazy);
+extern void pv_sched_vcpu_kerncs_boost_lazy(int boost_type);
+#else
+static inline bool pv_sched_enabled(void)
+{
+	return false;
+}
+
+static inline void pv_sched_enable(void) { }
+
+static inline void pv_sched_vcpu_update(int policy, int prio,
+		int nice, bool lazy)
+{
+}
+static inline void pv_sched_vcpu_kerncs_unboost(int boost_type, bool lazy) { }
+static inline void pv_sched_vcpu_kerncs_boost_lazy(int boost_type) { }
+#endif
 #endif
