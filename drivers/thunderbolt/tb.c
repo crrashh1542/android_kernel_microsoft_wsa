8205bf46929b10a475809d20195eb5bc666fb793
Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
2023-10-22 01:36:12 +0000

    UPSTREAM: thunderbolt: Call tb_switch_put() once DisplayPort bandwidth request is finished
    When handling DisplayPort bandwidth request tb_switch_find_by_route() is
    called and it returns a router structure with reference count increased.
    In order to avoid resource leak call tb_switch_put() when finished.
    
    Fixes: 6ce3563520be ("thunderbolt: Add support for DisplayPort bandwidth allocation mode")
    Cc: stable@vger.kernel.org
    Signed-off-by: Gil Fine <gil.fine@linux.intel.com>
    Signed-off-by: Mika Westerberg <mika.westerberg@linux.intel.com>
    (cherry picked from commit ec4405ed92036f5bb487b5c2f9a28f9e36a3e3d5)
    
    BUG=b:294519146
    TEST=TBT3/4 functional tests to ensure backward compatibility
    
    Signed-off-by: Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
    Change-Id: Ic7b3f72c431ba7dd653f433e9504239a288f7838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4962783
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Commit-Queue: Guenter Roeck <groeck@chromium.org>

--- drivers/thunderbolt/tb.c
+++ drivers/thunderbolt/tb.c
@@ -1771,14 +1771,14 @@ static void tb_handle_dp_bandwidth_request(struct work_struct *work)
 	in = &sw->ports[ev->port];
 	if (!tb_port_is_dpin(in)) {
 		tb_port_warn(in, "bandwidth request to non-DP IN adapter\n");
-		goto unlock;
+		goto put_sw;
 	}
 
 	tb_port_dbg(in, "handling bandwidth allocation request\n");
 
 	if (!usb4_dp_port_bw_mode_enabled(in)) {
 		tb_port_warn(in, "bandwidth allocation mode not enabled\n");
-		goto unlock;
+		goto put_sw;
 	}
 
 	ret = usb4_dp_port_requested_bw(in);
@@ -1787,7 +1787,7 @@ static void tb_handle_dp_bandwidth_request(struct work_struct *work)
 			tb_port_dbg(in, "no bandwidth request active\n");
 		else
 			tb_port_warn(in, "failed to read requested bandwidth\n");
-		goto unlock;
+		goto put_sw;
 	}
 	requested_bw = ret;
 
@@ -1796,7 +1796,7 @@ static void tb_handle_dp_bandwidth_request(struct work_struct *work)
 	tunnel = tb_find_tunnel(tb, TB_TUNNEL_DP, in, NULL);
 	if (!tunnel) {
 		tb_port_warn(in, "failed to find tunnel\n");
-		goto unlock;
+		goto put_sw;
 	}
 
 	out = tunnel->dst_port;
@@ -1823,6 +1823,8 @@ static void tb_handle_dp_bandwidth_request(struct work_struct *work)
 		tb_recalc_estimated_bandwidth(tb);
 	}
 
+put_sw:
+	tb_switch_put(sw);
 unlock:
 	mutex_unlock(&tb->lock);
 
