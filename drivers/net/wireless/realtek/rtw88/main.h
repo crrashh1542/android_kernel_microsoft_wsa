2f5533d3737285cbf2ac83243c653b6ee4611369
Chen-Yu Tsai <wenst@chromium.org>
2023-10-23 16:52:43 +0800

    Revert "CHROMIUM: rtw88: sar: add SAR of TX power limit"
    This reverts commit cc117192ce4f7465d36833f10565a6dbdd11f462.
    
    BUG=b:211772409
    TEST=none
    
    Change-Id: I40e3f902ba5d7cd1ff9b555e8d5c293ff4aec389
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4964670
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>

--- drivers/net/wireless/realtek/rtw88/main.h
+++ drivers/net/wireless/realtek/rtw88/main.h
@@ -414,13 +414,6 @@ enum rtw_wow_flags {
 	RTW_WOW_FLAG_MAX,
 };
 
-enum rtw_sar_sources {
-	RTW_SAR_SOURCE_NONE,
-	RTW_SAR_SOURCE_VNDCMD,
-	RTW_SAR_SOURCE_ACPI_STATIC,
-	RTW_SAR_SOURCE_ACPI_DYNAMIC,
-};
-
 /* the power index is represented by differences, which cck-1s & ht40-1s are
  * the base values, so for 1s's differences, there are only ht20 & ofdm
  */
@@ -1830,10 +1823,6 @@ struct rtw_fw_state {
 	u32 feature_ext;
 };
 
-struct rtw_sar {
-	enum rtw_sar_sources source;
-};
-
 struct rtw_hal {
 	u32 rcr;
 
@@ -1881,10 +1870,6 @@ struct rtw_hal {
 			  [RTW_CHANNEL_WIDTH_MAX]
 			  [RTW_RATE_SECTION_MAX]
 			  [RTW_MAX_CHANNEL_NUM_5G];
-	s8 tx_pwr_sar_2g[RTW_REGD_MAX][RTW_RF_PATH_MAX][RTW_RATE_SECTION_MAX]
-			[RTW_MAX_CHANNEL_NUM_2G];
-	s8 tx_pwr_sar_5g[RTW_REGD_MAX][RTW_RF_PATH_MAX][RTW_RATE_SECTION_MAX]
-			[RTW_MAX_CHANNEL_NUM_5G];
 	s8 tx_pwr_tbl[RTW_RF_PATH_MAX]
 		     [DESC_RATE_MAX];
 };
@@ -2002,8 +1987,6 @@ struct rtw_dev {
 	bool need_rfk;
 	struct completion fw_scan_density;
 	bool ap_active;
-
-	struct rtw_sar sar;
 
 	/* hci related data, must be last */
 	u8 priv[] __aligned(sizeof(void *));
