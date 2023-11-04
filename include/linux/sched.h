bee2541c30b0eaa666addcf24e999476a0307769
Peter Zijlstra <peterz@infradead.org>
2023-11-04 11:59:21 +0100

    BACKPORT: FROMGIT: sched/deadline: Introduce deadline servers
    Low priority tasks (e.g., SCHED_OTHER) can suffer starvation if tasks
    with higher priority (e.g., SCHED_FIFO) monopolize CPU(s).
    
    RT Throttling has been introduced a while ago as a (mostly debug)
    countermeasure one can utilize to reserve some CPU time for low priority
    tasks (usually background type of work, e.g. workqueues, timers, etc.).
    It however has its own problems (see documentation) and the undesired
    effect of unconditionally throttling FIFO tasks even when no lower
    priority activity needs to run (there are mechanisms to fix this issue
    as well, but, again, with their own problems).
    
    Introduce deadline servers to service low priority tasks needs under
    starvation conditions. Deadline servers are built extending SCHED_DEADLINE
    implementation to allow 2-level scheduling (a sched_deadline entity
    becomes a container for lower priority scheduling entities).
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Daniel Bristot de Oliveira <bristot@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/4968601859d920335cf85822eb573a5f179f04b8.1699095159.git.bristot@kernel.org
    
    (cherry picked from commit 63ba8422f876e32ee564ea95da9a7313b13ff0a1 https://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git sched/core)
    
     Conflicts:
    	include/linux/sched.h
    	kernel/sched/deadline.c
    
    BUG=b:307273029
    TEST=booted
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    
    Change-Id: I841c838f1586a4ed4b71a4ec77fb2c14e00a9e08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5083114
    Tested-by: Suleiman Souhlal <suleiman@google.com>
    Reviewed-by: Masami Hiramatsu <mhiramat@google.com>
    Reviewed-by: Joel Fernandes <joelaf@google.com>
    Commit-Queue: Suleiman Souhlal <suleiman@google.com>
    Tested-by: Joel Fernandes <joelaf@google.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -61,12 +61,14 @@ struct robust_list_head;
 struct root_domain;
 struct rq;
 struct sched_attr;
+struct sched_dl_entity;
 struct sched_param;
 struct seq_file;
 struct sighand_struct;
 struct signal_struct;
 struct task_delay_info;
 struct task_group;
+struct task_struct;
 
 /*
  * Task state bitmask. NOTE! These bits are also
@@ -589,6 +591,9 @@ struct sched_rt_entity {
 #endif
 } __randomize_layout;
 
+typedef bool (*dl_server_has_tasks_f)(struct sched_dl_entity *);
+typedef struct task_struct *(*dl_server_pick_f)(struct sched_dl_entity *);
+
 struct sched_dl_entity {
 	struct rb_node			rb_node;
 
@@ -640,6 +645,7 @@ struct sched_dl_entity {
 	unsigned int			dl_yielded        : 1;
 	unsigned int			dl_non_contending : 1;
 	unsigned int			dl_overrun	  : 1;
+	unsigned int			dl_server         : 1;
 
 	/*
 	 * Bandwidth enforcement timer. Each -deadline task has its
@@ -654,7 +660,20 @@ struct sched_dl_entity {
 	 * timer is needed to decrease the active utilization at the correct
 	 * time.
 	 */
-	struct hrtimer inactive_timer;
+	struct hrtimer			inactive_timer;
+
+	/*
+	 * Bits for DL-server functionality. Also see the comment near
+	 * dl_server_update().
+	 *
+	 * @rq the runqueue this server is for
+	 *
+	 * @server_has_tasks() returns true if @server_pick return a
+	 * runnable task.
+	 */
+	struct rq			*rq;
+	dl_server_has_tasks_f		server_has_tasks;
+	dl_server_pick_f		server_pick;
 
 #ifdef CONFIG_RT_MUTEXES
 	/*
@@ -788,6 +807,7 @@ struct task_struct {
 	struct sched_entity		se;
 	struct sched_rt_entity		rt;
 	struct sched_dl_entity		dl;
+	struct sched_dl_entity		*dl_server;
 
 #ifdef CONFIG_SCHED_CORE
 	struct rb_node			core_node;
