ffd11a93d469a6011032e54ac0075e0db2ac402d
Stéphane Marchesin <marcheu@chromium.org>
2023-11-06 18:12:51 -0800

    CHROMIUM: drm/i915: Also disable RC6 on EHL
    These platforms suffer from the same issue as JSL.
    
    BUG=b:303841978
    UPSTREAM-TASK=b:307840654
    TEST=builds
    
    Signed-off-by: Stéphane Marchesin <marcheu@chromium.org>
    Change-Id: I6ada31059d987f6dd86b08772f32fbd2300e1060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5042442
    Reviewed-by: Ilja Friedel <ihf@chromium.org>

--- drivers/gpu/drm/i915/gt/intel_rc6.c
+++ drivers/gpu/drm/i915/gt/intel_rc6.c
@@ -510,12 +510,10 @@ static bool rc6_supported(struct intel_rc6 *rc6)
 	 *
 	 * TODO: enable software RC6 contorl after a fix is found.
 	 */
-	if (INTEL_GEN(i915) == 11) {
-		if (IS_PLATFORM(i915, INTEL_JASPERLAKE)) {
-			drm_notice(&i915->drm,
-			   "WA: Software RC6 disabled for Jasperlake platform\n");
-			return false;
-		}
+	if (IS_JSL_EHL(i915)) {
+		drm_notice(&i915->drm,
+		   "WA: Software RC6 disabled for Jasperlake/Elkhartlake platforms\n");
+		return false;
 	}
 
 	return true;
