440adaaddb7bf6c3396b26b897d7474f0b34e11e
Terry Cheong <htcheong@chromium.org>
2023-11-08 07:56:25 +0000

    Revert "CHROMIUM: ASoC: Intel: Add rpl_mx98360_nau8825 driver"
    This reverts commit 57fad1188063a4df93519a8ade471524d4efbb5a.
    
    Reason for revert: Landed on upstream
    
    Original change's description:
    > CHROMIUM: ASoC: Intel: Add rpl_mx98360_nau8825 driver
    >
    > BUG=b:297253663
    > TEST=audio use cases
    > UPSTREAM-TASK=b:305898342
    >
    > Change-Id: I32f5592c86024cfac322009ec74c2cdf0fabc642
    > Signed-off-by: Terry Cheong <htcheong@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4942920
    > Commit-Queue: Curtis Malainey <cujomalainey@chromium.org>
    > Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    > Tested-by: ChiJen Chen <chi-jen.chen@quanta.corp-partner.google.com>
    
    BUG=b:297253663,b:305898342
    
    Cq-Depend: chromium:5012274
    Change-Id: I7b469aba822ca6484d6b7e024f3f967fc564c45d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5009778
    Tested-by: Terry Cheong <htcheong@chromium.org>
    Commit-Queue: Terry Cheong <htcheong@chromium.org>
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>

--- sound/soc/intel/boards/sof_nau8825.c
+++ sound/soc/intel/boards/sof_nau8825.c
@@ -665,16 +665,6 @@ static const struct platform_device_id board_ids[] = {
 					SOF_SSP_BT_OFFLOAD_PRESENT),
 	},
 	{
-		.name = "adl_nau8318_8825",
-		.driver_data = (kernel_ulong_t)(SOF_NAU8825_SSP_CODEC(0) |
-					SOF_SPEAKER_AMP_PRESENT |
-					SOF_NAU8318_SPEAKER_AMP_PRESENT |
-					SOF_NAU8825_SSP_AMP(1) |
-					SOF_NAU8825_NUM_HDMIDEV(4) |
-					SOF_BT_OFFLOAD_SSP(2) |
-					SOF_SSP_BT_OFFLOAD_PRESENT),
-	},
-	{
 		.name = "rpl_max98373_8825",
 		.driver_data = (kernel_ulong_t)(SOF_NAU8825_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |
@@ -685,10 +675,10 @@ static const struct platform_device_id board_ids[] = {
 					SOF_SSP_BT_OFFLOAD_PRESENT),
 	},
 	{
-		.name = "rpl_mx98360a_8825",
+		.name = "adl_nau8318_8825",
 		.driver_data = (kernel_ulong_t)(SOF_NAU8825_SSP_CODEC(0) |
 					SOF_SPEAKER_AMP_PRESENT |
-					SOF_MAX98360A_SPEAKER_AMP_PRESENT |
+					SOF_NAU8318_SPEAKER_AMP_PRESENT |
 					SOF_NAU8825_SSP_AMP(1) |
 					SOF_NAU8825_NUM_HDMIDEV(4) |
 					SOF_BT_OFFLOAD_SSP(2) |
