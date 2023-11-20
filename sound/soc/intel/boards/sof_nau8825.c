6d61748350ece8908ced89a06babd514e616f97f
Terry Cheong <htcheong@chromium.org>
2023-11-20 10:02:09 +0000

    FIXUP: UPSTREAM: ASoC: Intel: sof_nau8825: add RPL support for MAX98360A amp
    The upstream patch is created after `SOF_MAX98360A_SPEAKER_AMP_PRESENT` quirks are removed.
    Add them back in the lookup table to fix audio issues.
    
    BUG=b:311553248
    TEST=check audio probe on osiris
    
    Change-Id: Iedac3b680c3b15803f6da1db9d0308761f8d1abd
    Signed-off-by: Terry Cheong <htcheong@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5043669
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Commit-Queue: Curtis Malainey <cujomalainey@chromium.org>

--- sound/soc/intel/boards/sof_nau8825.c
+++ sound/soc/intel/boards/sof_nau8825.c
@@ -687,6 +687,8 @@ static const struct platform_device_id board_ids[] = {
 	{
 		.name = "rpl_mx98360a_8825",
 		.driver_data = (kernel_ulong_t)(SOF_NAU8825_SSP_CODEC(0) |
+					SOF_SPEAKER_AMP_PRESENT |
+					SOF_MAX98360A_SPEAKER_AMP_PRESENT |
 					SOF_NAU8825_SSP_AMP(1) |
 					SOF_NAU8825_NUM_HDMIDEV(4) |
 					SOF_BT_OFFLOAD_SSP(2) |
