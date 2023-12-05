b595626be33532b1bf287515b261db6888f46f86
Youssef Esmat <youssefesmat@chromium.org>
2023-12-05 12:03:23 -0600

    BACKPORT: sched/fair: Propagate enqueue flags into place_entity()
    This allows place_entity() to consider ENQUEUE_WAKEUP and
    ENQUEUE_MIGRATED.
    
    BUG=b:308209790
    TEST=Manual tested on DUT
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20230531124604.274010996@infradead.org
    Signed-off-by: Youssef Esmat <youssefesmat@chromium.org>
    
    Change-Id: Ie2403b6ac11bf3e0f8b489e4d10ae2558c0bcf0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5089900
    Reviewed-by: Vineeth Pillai <vineethrp@google.com>
    Commit-Queue: Youssef Esmat <youssefesmat@chromium.org>
    Tested-by: Vineeth Pillai <vineethrp@google.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2155,6 +2155,7 @@ extern const u32		sched_prio_to_wmult[40];
 #else
 #define ENQUEUE_MIGRATED	0x00
 #endif
+#define ENQUEUE_INITIAL		0x80
 #define ENQUEUE_MIGRATING	0x100
 
 #define RETRY_TASK		((void *)-1UL)
