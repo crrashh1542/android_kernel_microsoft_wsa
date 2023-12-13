7318fbc82d6788237911741f3e3cc15424d1d6e3
Vineeth Pillai (Google) <vineeth@bitbyteword.org>
2023-12-13 10:24:51 -0500

    CHROMIUM: kvm: x86: boost vcpu threads on latency sensitive paths
    Proactively boost the vcpu thread when delivering an interrupt so
    that the guest vcpu gets to run with minimum latency and service the
    interrupt. The host knows that guest vcpu is going to service an irq/nmi
    as soon as its delivered and boosting the priority will help the guest
    to avoid latencies. Timer interrupt is one common scenario which
    benefits from this.
    
    When a vcpu resumes from halt, it would be because of an event like
    timer or irq/nmi and is latency sensitive. So, boosting the priority
    of vcpu thread when it goes idle makes sense as the wakeup would be
    because of a latency sensitive event and this boosting will not hurt
    the host as the thread is scheduled out.
    
    UPSTREAM-TASK=b:303645537
    BUG=b:262267726
    TEST=boot
    
    Co-developed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Signed-off-by: Vineeth Pillai (Google) <vineeth@bitbyteword.org>
    
    Change-Id: Ibb00c30f76367cbbf3951e3f46a6b01fc97d9c31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5249684
    Reviewed-by: Joel Fernandes <joelaf@google.com>
    Tested-by: Vineeth Pillai <vineethrp@google.com>
    Commit-Queue: Vineeth Pillai <vineethrp@google.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -1099,7 +1099,7 @@ static int __apic_accept_irq(struct kvm_lapic *apic, int delivery_mode,
 		if (static_call(kvm_x86_deliver_posted_interrupt)(vcpu, vector)) {
 			kvm_lapic_set_irr(vector, apic);
 			kvm_make_request(KVM_REQ_EVENT, vcpu);
-			kvm_vcpu_kick(vcpu);
+			kvm_vcpu_kick_boost(vcpu);
 		}
 		break;
 
@@ -1107,19 +1107,19 @@ static int __apic_accept_irq(struct kvm_lapic *apic, int delivery_mode,
 		result = 1;
 		vcpu->arch.pv.pv_unhalted = 1;
 		kvm_make_request(KVM_REQ_EVENT, vcpu);
-		kvm_vcpu_kick(vcpu);
+		kvm_vcpu_kick_boost(vcpu);
 		break;
 
 	case APIC_DM_SMI:
 		result = 1;
 		kvm_make_request(KVM_REQ_SMI, vcpu);
-		kvm_vcpu_kick(vcpu);
+		kvm_vcpu_kick_boost(vcpu);
 		break;
 
 	case APIC_DM_NMI:
 		result = 1;
 		kvm_inject_nmi(vcpu);
-		kvm_vcpu_kick(vcpu);
+		kvm_vcpu_kick_boost(vcpu);
 		break;
 
 	case APIC_DM_INIT:
@@ -1683,7 +1683,7 @@ static void apic_timer_expired(struct kvm_lapic *apic, bool from_timer_fn)
 	atomic_inc(&apic->lapic_timer.pending);
 	kvm_make_request(KVM_REQ_UNBLOCK, vcpu);
 	if (from_timer_fn)
-		kvm_vcpu_kick(vcpu);
+		kvm_vcpu_kick_boost(vcpu);
 }
 
 static void start_sw_tscdeadline(struct kvm_lapic *apic)
