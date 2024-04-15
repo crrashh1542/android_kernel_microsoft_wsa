fc204e7767627dca58a81874697fe0e092e88cd9
crrashh1542 <kotsuki@crrashh.com>
2024-04-16 01:31:46 +0800

    kernelsu: enable complete features
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- fs/open.c
+++ fs/open.c
@@ -340,6 +340,12 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return ksys_fallocate(fd, mode, offset, len);
 }
 
+/* Support for KernelSU #1 */
+#ifdef CONFIG_KSU
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			        int *flags);
+#endif
+
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
